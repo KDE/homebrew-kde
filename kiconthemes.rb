1f0edb759e82eccbdf50f9c569053c165ec2e14f
Harald Fernengel <harald.fernengel@nokia.com>
2014-02-05 22:57:45 +0100

    add kiconthemes.rb

--- kiconthemes.rb
+++ kiconthemes.rb
@@ -1,25 +1,20 @@
 require "formula"
 
-class Kio < Formula
+class Kiconthemes < Formula
   homepage "http://www.kde.org/"
-  url "http://download.kde.org/unstable/frameworks/4.95.0/kio-4.95.0.tar.xz"
+  url "http://download.kde.org/unstable/frameworks/4.95.0/kiconthemes-4.95.0.tar.xz"
   sha1 ""
 
-  head 'git://anongit.kde.org/kio.git'
+  head 'git://anongit.kde.org/kiconthemes.git'
 
   depends_on "cmake" => :build
   depends_on "haraldf/kf5/extra-cmake-modules" => :build
   depends_on "qt5"
-  depends_on "haraldf/kf5/kdbusaddons"
-  depends_on "haraldf/kf5/kitemviews"
-  depends_on "haraldf/kf5/kservice"
-  depends_on "haraldf/kf5/kconfigwidgets"
-  depends_on "haraldf/kf5/kf5-solid"
-  depends_on "haraldf/kf5/kiconthemes"
 
   def install
     args = std_cmake_args
     args << "-DCMAKE_PREFIX_PATH=\"#{Formula.factory('qt5').opt_prefix};#{Formula.factory('extra-cmake-modules').opt_prefix}\""
+    args << "-DCMAKE_CXX_FLAGS='-D_DARWIN_C_SOURCE'"
 
     system "cmake", ".", *args
     system "make", "install"
