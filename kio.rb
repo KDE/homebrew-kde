7b66b79e2ee4253908bc4b17551a14729382491d
Harald Fernengel <harald.fernengel@nokia.com>
2014-02-06 16:06:43 +0100

    more formula

--- kio.rb
+++ kio.rb
@@ -16,6 +16,11 @@ class Kio < Formula
   depends_on "haraldf/kf5/kconfigwidgets"
   depends_on "haraldf/kf5/kf5-solid"
   depends_on "haraldf/kf5/kiconthemes"
+  depends_on "haraldf/kf5/kcompletion"
+
+  def patches
+    DATA
+  end
 
   def install
     args = std_cmake_args
@@ -25,3 +30,26 @@ class Kio < Formula
     system "make", "install"
   end
 end
+
+__END__
+diff --git a/CMakeLists.txt b/CMakeLists.txt
+index 0e67517..f0dafe6 100644
+--- a/CMakeLists.txt
++++ b/CMakeLists.txt
+@@ -40,12 +40,12 @@ if("${CMAKE_BINARY_DIR}" STREQUAL "${CMAKE_CURRENT_BINARY_DIR}")
+   find_package(KF5Solid ${KF5_VERSION} REQUIRED)
+   find_package(KF5JobWidgets ${KF5_VERSION} REQUIRED)
+   find_package(KF5IconThemes ${KF5_VERSION} REQUIRED)
+-  find_package(KF5WindowSystem ${KF5_VERSION} REQUIRED)
++# find_package(KF5WindowSystem ${KF5_VERSION} REQUIRED)
+   find_package(KF5Crash ${KF5_VERSION} REQUIRED)
+   find_package(KF5Completion ${KF5_VERSION} REQUIRED)
+-  find_package(KF5DocTools ${KF5_VERSION} REQUIRED)
+-  find_package(KF5TextWidgets ${KF5_VERSION} REQUIRED)
+-  find_package(KF5XmlGui ${KF5_VERSION} REQUIRED)
++# find_package(KF5DocTools ${KF5_VERSION} REQUIRED)
++#  find_package(KF5TextWidgets ${KF5_VERSION} REQUIRED)
++#  find_package(KF5XmlGui ${KF5_VERSION} REQUIRED)
+   find_package(KF5Bookmarks ${KF5_VERSION} REQUIRED)
+   find_package(KF5Notifications ${KF5_VERSION} REQUIRED) #needed for kpac
+ else()
