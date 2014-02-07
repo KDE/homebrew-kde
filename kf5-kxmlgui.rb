998b2e563746412459f7153829a302a9ba5b5360
Harald Fernengel <harry@kdevelop.org>
2014-02-07 13:30:41 +0100

    add kf5-kxmlgui.rb

---
+++ kf5-kxmlgui.rb
@@ -0,0 +1,28 @@
+require "formula"
+
+class Kf5Kxmlgui < Formula
+
+  head 'git://anongit.kde.org/kxmlgui.git'
+
+  depends_on "cmake" => :build
+  depends_on "haraldf/kf5/kf5-extra-cmake-modules" => :build
+  depends_on "haraldf/kf5/kf5-kitemviews"
+  depends_on "haraldf/kf5/kf5-kconfig"
+  depends_on "haraldf/kf5/kf5-kglobalaccel"
+  depends_on "haraldf/kf5/kf5-kconfigwidgets"
+  depends_on "haraldf/kf5/kf5-ki18n"
+  depends_on "haraldf/kf5/kf5-kiconthemes"
+  depends_on "haraldf/kf5/kf5-ktextwidgets"
+  depends_on "haraldf/kf5/kf5-kwidgetsaddons"
+  depends_on "haraldf/kf5/kf5-kwindowsystem"
+  depends_on "haraldf/kf5/kf5-attica"
+  depends_on "qt5"
+
+  def install
+    args = std_cmake_args
+    args << "-DCMAKE_PREFIX_PATH=\"#{Formula.factory('qt5').opt_prefix};#{Formula.factory('kf5-extra-cmake-modules').opt_prefix}\""
+
+    system "cmake", ".", *args
+    system "make", "install"
+  end
+end
