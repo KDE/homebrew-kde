f4821da2ec8766dd1d845165f8df0dab078d0c64
Harald Fernengel <harry@kdevelop.org>
2014-02-07 21:43:17 +0100

    redo patch again

--- kf5-kio.rb
+++ kf5-kio.rb
@@ -56,3 +56,57 @@ index 0e67517..8eaae80 100644
  include(CheckLibraryExists)
  add_subdirectory(src)
  add_subdirectory(autotests)
+diff --git a/src/CMakeLists.txt b/src/CMakeLists.txt
+index 2106e1f..1b5fa82 100644
+--- a/src/CMakeLists.txt
++++ b/src/CMakeLists.txt
+@@ -55,7 +55,7 @@ add_subdirectory(ioslaves)
+ add_subdirectory(kssld)
+ add_subdirectory(kioslave)
+ 
+-if(NOT WIN32)
++if(NOT WIN32 AND NOT APPLE)
+   # arpa/nameser.h
+   add_subdirectory(kpac)
+ endif()
+diff --git a/src/core/kmountpoint.cpp b/src/core/kmountpoint.cpp
+index cc69957..3d568f8 100644
+--- a/src/core/kmountpoint.cpp
++++ b/src/core/kmountpoint.cpp
+@@ -51,6 +51,12 @@ static Qt::CaseSensitivity cs = Qt::CaseSensitive;
+ #include <sys/mntent.h>
+ #endif
+ 
++#ifdef Q_OS_MAC
++#  define HAVE_SYS_MOUNT_H 1
++#  define HAVE_SYS_TYPES_H 1
++#  define HAVE_SYS_PARAM_H 1
++#endif
++
+ // This is the *BSD branch
+ #if HAVE_SYS_MOUNT_H
+ #if HAVE_SYS_TYPES_H
+diff --git a/src/ioslaves/CMakeLists.txt b/src/ioslaves/CMakeLists.txt
+index 6436b12..b848c66 100644
+--- a/src/ioslaves/CMakeLists.txt
++++ b/src/ioslaves/CMakeLists.txt
+@@ -1,6 +1,6 @@
+ 
+ add_subdirectory( file )
+-add_subdirectory( help )
++#add_subdirectory( help )
+ add_subdirectory( http )
+ add_subdirectory( ftp )
+ add_subdirectory( mailto )
+diff --git a/src/ioslaves/file/file.cpp b/src/ioslaves/file/file.cpp
+index b7b9296..525e1ff 100644
+--- a/src/ioslaves/file/file.cpp
++++ b/src/ioslaves/file/file.cpp
+@@ -42,6 +42,7 @@
+ #include <utime.h>
+ #endif
+ 
++#include <errno.h>
+ 
+ #include <QtCore/QByteRef>
+ #include <QtCore/QDate>
