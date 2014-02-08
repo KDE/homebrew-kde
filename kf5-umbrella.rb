89f0fe42d1f02be80b455a37e4d5ff0dc1233259
Harald Fernengel <harry@kdevelop.org>
2014-02-08 22:04:17 +0100

    kf5-umbrella must not be keg-only

--- kf5-umbrella.rb
+++ kf5-umbrella.rb
@@ -5,8 +5,6 @@ class Kf5Umbrella < Formula
 #  url "http://download.kde.org/unstable/frameworks/4.95.0/kf5umbrella-4.95.0.tar.xz"
 #  sha1 "5b7ed9d1e0007926d9d1a4752105e4602bc5addd"
 
-  keg_only "Only required for building KDE frameworks"
-
   head 'git://anongit.kde.org/kf5umbrella.git'
 
   depends_on "cmake" => :build
