6113c0ab2ea9006029511ecdec31baff078c661a
Harald Fernengel <harry@kdevelop.org>
2014-02-08 12:10:45 +0100

    fix ktexteditor

--- kf5-ktexteditor.rb
+++ kf5-ktexteditor.rb
@@ -17,10 +17,12 @@ class Kf5Ktexteditor < Formula
   depends_on "haraldf/kf5/kf5-kguiaddons"
   depends_on "haraldf/kf5/kf5-kjobwidgets"
   depends_on "haraldf/kf5/kf5-kio"
+  depends_on "haraldf/kf5/kf5-kparts"
 
   def install
     args = std_cmake_args
     args << "-DCMAKE_PREFIX_PATH=\"#{Formula.factory('qt5').opt_prefix};#{Formula.factory('kf5-extra-cmake-modules').opt_prefix}\""
+    args << "-DCMAKE_CXX_FLAGS='-D_DARWIN_C_SOURCE'"
 
     system "cmake", ".", *args
     system "make", "install"
