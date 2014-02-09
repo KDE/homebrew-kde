3179681e6118023841bee6422ef7fb85ef4011b0
Harald Fernengel <harry@kdevelop.org>
2014-02-09 18:49:16 +0100

    add kf5-kitemmodels

---
+++ kf5-kitemmodels.rb
@@ -0,0 +1,19 @@
+require "formula"
+
+class Kf5Kitemmodels < Formula
+  homepage "http://www.kde.org/"
+
+  head 'git://anongit.kde.org/kitemmodels.git'
+
+  depends_on "cmake" => :build
+  depends_on "haraldf/kf5/kf5-extra-cmake-modules" => :build
+  depends_on "qt5" => "with-d-bus"
+
+  def install
+    args = std_cmake_args
+    args << "-DCMAKE_PREFIX_PATH=\"#{Formula.factory('qt5').opt_prefix};#{Formula.factory('kf5-extra-cmake-modules').opt_prefix}\""
+
+    system "cmake", ".", *args
+    system "make", "install"
+  end
+end
