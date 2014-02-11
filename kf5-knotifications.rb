c710c97e9e5522f3a90c3fd7e5ce24d78ab956f5
Harald Fernengel <harry@kdevelop.org>
2014-02-11 14:02:07 +0100

    ignore X11

--- kf5-knotifications.rb
+++ kf5-knotifications.rb
@@ -8,6 +8,10 @@ class Kf5Knotifications < Formula
   depends_on "haraldf/kf5/kf5-extra-cmake-modules" => :build
   depends_on "qt5" => "with-d-bus"
 
+  def patches
+    DATA
+  end
+
   def install
     args = std_cmake_args
     args << "-DCMAKE_PREFIX_PATH=\"#{Formula.factory('qt5').opt_prefix};#{Formula.factory('kf5-extra-cmake-modules').opt_prefix}\""
@@ -16,3 +20,20 @@ class Kf5Knotifications < Formula
     system "make", "install"
   end
 end
+
+__END__
+diff --git a/CMakeLists.txt b/CMakeLists.txt
+index 63ebf71..f4cbf81 100644
+--- a/CMakeLists.txt
++++ b/CMakeLists.txt
+@@ -27,7 +27,9 @@ include(KDEInstallDirs)
+ include(KDEFrameworkCompilerSettings)
+ include(KDECMakeSettings)
+ 
+-find_package(X11)
++if (NOT APPLE)
++    find_package(X11)
++endif()
+ set(HAVE_X11 ${X11_FOUND})
+ set(HAVE_XTEST ${X11_XTest_FOUND})
+ if(X11_FOUND)
