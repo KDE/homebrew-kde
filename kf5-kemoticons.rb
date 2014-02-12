6bc7e120d30c2732a1ad289914a1df3c0cd40863
Harald Fernengel <harry@kdevelop.org>
2014-02-12 16:23:03 +0100

    add kemoticons

---
+++ kf5-kemoticons.rb
@@ -0,0 +1,27 @@
+require "formula"
+
+class Kf5Kemoticons < Formula
+  homepage "http://www.kde.org/"
+
+  head 'git://anongit.kde.org/kemoticons.git'
+
+  depends_on "cmake" => :build
+  depends_on "haraldf/kf5/kf5-extra-cmake-modules" => :build
+  depends_on "qt5" => "with-d-bus"
+
+  depends_on "haraldf/kf5/kf5-karchive"
+  depends_on "haraldf/kf5/kf5-kconfig"
+  depends_on "haraldf/kf5/kf5-kcoreaddons"
+  depends_on "haraldf/kf5/kf5-kdbusaddons"
+  depends_on "haraldf/kf5/kf5-kjs"
+  depends_on "haraldf/kf5/kf5-ki18n"
+  depends_on "haraldf/kf5/kf5-kservice"
+
+  def install
+    args = std_cmake_args
+    args << "-DCMAKE_PREFIX_PATH=\"#{Formula.factory('qt5').opt_prefix};#{Formula.factory('kf5-extra-cmake-modules').opt_prefix}\""
+
+    system "cmake", ".", *args
+    system "make", "install"
+  end
+end
