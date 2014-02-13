adebf9eda81b03396f1bad63c4c448ae08b687a3
Harald Fernengel <harry@kdevelop.org>
2014-02-13 09:03:02 +0100

    add kinit

---
+++ kf5-kinit.rb
@@ -0,0 +1,20 @@
+require "formula"
+
+class Kf5Kinit < Formula
+  homepage "http://www.kde.org/"
+
+  head 'git://anongit.kde.org/kinit.git'
+
+  depends_on "cmake" => :build
+  depends_on "haraldf/kf5/kf5-extra-cmake-modules" => :build
+  depends_on "qt5" => "with-d-bus"
+
+  def install
+    args = std_cmake_args
+    args << "-DCMAKE_PREFIX_PATH=\"#{Formula.factory('qt5').opt_prefix};#{Formula.factory('kf5-extra-cmake-modules').opt_prefix}\""
+    args << "-DCMAKE_CXX_FLAGS='-D_DARWIN_C_SOURCE'"
+
+    system "cmake", ".", *args
+    system "make", "install"
+  end
+end
