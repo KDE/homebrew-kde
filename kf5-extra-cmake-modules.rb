6bfb4755d92e456068913f1b4e17a80d03c19f02
Harald Fernengel <harry@kdevelop.org>
2014-02-14 19:36:20 +0100

    Update patch
    My compiler fix is now upstream.
    
    Change DATA_INSTALL_DIR to point to "~/Library/Application Support",
    as that is what QStandardPaths expects.

--- kf5-extra-cmake-modules.rb
+++ kf5-extra-cmake-modules.rb
@@ -22,8 +22,24 @@ class Kf5ExtraCmakeModules < Formula
 end
 
 __END__
+diff --git a/kde-modules/KDEInstallDirs.cmake b/kde-modules/KDEInstallDirs.cmake
+index 9ff2354..054e697 100644
+--- a/kde-modules/KDEInstallDirs.cmake
++++ b/kde-modules/KDEInstallDirs.cmake
+@@ -143,7 +143,11 @@ _set_fancy(PLUGIN_INSTALL_DIR        "${QT_PLUGIN_INSTALL_DIR}/kf5"
+ _set_fancy(IMPORTS_INSTALL_DIR       "${QT_PLUGIN_INSTALL_DIR}/imports"            "The install dir where QtQuick1 imports will be installed")
+ _set_fancy(QML_INSTALL_DIR           "${LIB_INSTALL_DIR}/qml"                      "The install dir where QtQuick2 imports will be installed")
+ _set_fancy(CMAKECONFIG_INSTALL_PREFIX "${LIB_INSTALL_DIR}/cmake"                   "The prefix under which packages will create their own subdirectory for their CMake configuration files")
++if (APPLE)
++_set_fancy(DATA_INSTALL_DIR          "$ENV{HOME}/Library/Application Support"                     "The parent directory where applications can install their data")
++else()
+ _set_fancy(DATA_INSTALL_DIR          "${SHARE_INSTALL_PREFIX}"                     "The parent directory where applications can install their data")
++endif()
+ _set_fancy(HTML_INSTALL_DIR          "${SHARE_INSTALL_PREFIX}/doc/HTML"            "The HTML install dir for documentation")
+ _set_fancy(ICON_INSTALL_DIR          "${SHARE_INSTALL_PREFIX}/icons"               "The icon install dir (default ${SHARE_INSTALL_PREFIX}/share/icons/)")
+ _set_fancy(KCFG_INSTALL_DIR          "${SHARE_INSTALL_PREFIX}/config.kcfg"         "The install dir for kconfig files")
 diff --git a/modules/ECMGenerateHeaders.cmake b/modules/ECMGenerateHeaders.cmake
-index e98a22e91151d23d7c798ff22a33097ec2a59d10..f296339137e82e8f719db0ac0d249af02143eca9 100644
+index e98a22e..f296339 100644
 --- a/modules/ECMGenerateHeaders.cmake
 +++ b/modules/ECMGenerateHeaders.cmake
 @@ -76,7 +76,7 @@ function(ECM_GENERATE_HEADERS)
@@ -35,16 +51,3 @@ index e98a22e91151d23d7c798ff22a33097ec2a59d10..f296339137e82e8f719db0ac0d249af0
              if (NOT EXISTS ${REGULAR_HEADER_NAME})
                  file(WRITE ${REGULAR_HEADER_NAME} "#include \"${_actualheader}\"\n")
              endif()
-diff --git a/kde-modules/KDECompilerSettings.cmake b/kde-modules/KDECompilerSettings.cmake
-index 335e127..3b546c5 100644
---- a/kde-modules/KDECompilerSettings.cmake
-+++ b/kde-modules/KDECompilerSettings.cmake
-@@ -273,7 +273,7 @@ endfunction()
- ############################################################
- 
- if ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU" OR
--        "${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang" OR
-+        ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang" AND NOT APPLE) OR
-         ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Intel" AND NOT WIN32))
-     # Linker warnings should be treated as errors
-     set(CMAKE_SHARED_LINKER_FLAGS "-Wl,--fatal-warnings ${CMAKE_SHARED_LINKER_FLAGS}")
