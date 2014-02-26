5c3f1b4e308dab86563324b02df2f340abfe112f
Harald Fernengel <harry@kdevelop.org>
2014-02-26 19:54:44 +0100

    add oxygen icons formula

---
+++ oxygen-icons.rb
@@ -0,0 +1,25 @@
+require "formula"
+
+class OxygenIcons < Formula
+  homepage "http://www.kde.org/"
+  url "http://download.kde.org/stable/4.12.0/src/oxygen-icons-4.12.0.tar.xz"
+  sha1 "4bb0de73e52d237c906903c383c2e36cb2d5c143"
+
+  depends_on "cmake" => :build
+
+  def install
+    args = std_cmake_args
+
+    system "cmake", ".", *args
+    system "make", "install"
+    ln_sf Dir["#{HOMEBREW_PREFIX}/share/icons"], "#{ENV['HOME']}/Library/Application Support/"
+  end
+
+  def caveats; <<-EOS.undent
+      A symlink "#{ENV['HOME']}/Library/Application Support/icons" was created
+      So that KF5 can find the oxygen themes.
+
+      This symlink can be removed when this formula is uninstalled.
+  EOS
+  end
+end
