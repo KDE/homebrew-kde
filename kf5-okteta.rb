683727630d584f87f49c6568500554d08696fe6c
Harald Fernengel <harry@kdevelop.org>
2014-03-19 11:21:47 +0100

    add HEAD-only formula for okteta

---
+++ kf5-okteta.rb
@@ -0,0 +1,56 @@
+require "formula"
+
+class Kf5Okteta < Formula
+  homepage "http://www.kde.org/"
+
+  head 'git://anongit.kde.org/okteta.git', :branch => 'frameworks'
+
+  depends_on "cmake" => :build
+  depends_on "haraldf/kf5/kf5-extra-cmake-modules" => :build
+  depends_on "qt5" => "with-d-bus"
+
+  depends_on "haraldf/kf5/kf5-umbrella"
+  depends_on "haraldf/kf5/kf5-kbookmarks"
+  depends_on "haraldf/kf5/kf5-kcodecs"
+  depends_on "haraldf/kf5/kf5-kcompletion"
+  depends_on "haraldf/kf5/kf5-kconfigwidgets"
+  depends_on "haraldf/kf5/kf5-kdbusaddons"
+  depends_on "haraldf/kf5/kf5-kdoctools"
+  depends_on "haraldf/kf5/kf5-ki18n"
+  depends_on "haraldf/kf5/kf5-kcmutils"
+  depends_on "haraldf/kf5/kf5-kio"
+  depends_on "haraldf/kf5/kf5-knewstuff"
+  depends_on "haraldf/kf5/kf5-kparts"
+  depends_on "haraldf/kf5/kf5-kservice"
+  depends_on "haraldf/kf5/kf5-solid"
+  depends_on "haraldf/kf5/kf5-kwidgetsaddons"
+  depends_on "haraldf/kf5/kf5-kxmlgui"
+
+  def patches
+    DATA
+  end
+
+  def install
+    args = std_cmake_args
+
+    args << "-DCMAKE_CXX_FLAGS='-D_DARWIN_C_SOURCE'"
+
+    system "cmake", ".", *args
+    system "make", "install"
+  end
+end
+
+__END__
+diff --git a/CMakeLists.txt b/CMakeLists.txt
+index e25a4b7..98258c0 100644
+--- a/CMakeLists.txt
++++ b/CMakeLists.txt
+@@ -91,7 +91,7 @@ add_definitions(
+ 
+ # helper libs
+ add_subdirectory( libs )
+-add_subdirectory( doc )
++#add_subdirectory( doc )
+ 
+ set( OKTETALIBS_MAJOR_VERSION 0 )
+ set( OKTETALIBS_MINOR_VERSION 9 )
