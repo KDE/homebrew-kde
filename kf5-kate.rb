5533ee78507b7586569945a14d06b0a7210a70cd
Harald Fernengel <harry@kdevelop.org>
2014-08-01 20:13:56 +0200

    master is now kf5 based

--- kf5-kate.rb
+++ kf5-kate.rb
@@ -3,7 +3,7 @@ require "formula"
 class Kf5Kate < Formula
   homepage "http://www.kde.org/"
 
-  head 'git://anongit.kde.org/kate.git', :branch => 'frameworks'
+  head 'git://anongit.kde.org/kate.git'
 
   depends_on "cmake" => :build
   depends_on "haraldf/kf5/kf5-extra-cmake-modules" => :build
