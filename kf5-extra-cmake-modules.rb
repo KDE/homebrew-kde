3c9a0b4c0a0e1793723e4f0983c44d21d0b004bf
Harald Fernengel <harry@kdevelop.org>
2014-08-09 22:43:02 +0200

    also symlink kxmlgui5 directory

--- kf5-extra-cmake-modules.rb
+++ kf5-extra-cmake-modules.rb
@@ -19,5 +19,6 @@ class Kf5ExtraCmakeModules < Formula
     ### Hack - QStandardDirs only looks in $HOME/Library/Application Support/, but kf5 installs
     ### to share. Make a symlink
     ln_sf Dir["#{HOMEBREW_PREFIX}/share/kf5"], "#{ENV['HOME']}/Library/Application Support/"
+    ln_sf Dir["#{HOMEBREW_PREFIX}/share/kxmlgui5"], "#{ENV['HOME']}/Library/Application Support/"
   end
 end
