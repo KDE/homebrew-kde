cfa58a2c594392627bbf9b1a172cd4661a088c66
Yue Liu <yue.liu@mail.com>
2014-11-27 15:07:24 -0800

    add kdesu

---
+++ kf5-kdesu.rb
@@ -0,0 +1,23 @@
+require "formula"
+
+class Kf5Kdesu < Formula
+  url "http://download.kde.org/stable/frameworks/5.4/kdesu-5.4.0.tar.xz"
+  sha1 "bbe8d7700ba1d5ca77068ddb45444e1163283063"
+  homepage "http://www.kde.org/"
+
+  head 'git://anongit.kde.org/kdesu.git'
+
+  depends_on "cmake" => :build
+  depends_on "haraldf/kf5/kf5-extra-cmake-modules" => :build
+  depends_on "qt5" => "with-d-bus"
+  depends_on "gettext" => :build
+
+  def install
+    args = std_cmake_args
+    args << "-DCMAKE_CXX_FLAGS='-D_DARWIN_C_SOURCE'"
+
+    system "cmake", ".", *args
+    system "make", "install"
+    prefix.install "install_manifest.txt"
+  end
+end
