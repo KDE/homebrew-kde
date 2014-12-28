f861de14eb6117df71fe9635df88faa9e953d242
Yue Liu <yue.liu@mail.com>
2014-12-28 01:40:28 -0800

    fix build without X11

--- kf5-kdesu.rb
+++ kf5-kdesu.rb
@@ -12,6 +12,10 @@ class Kf5Kdesu < Formula
   depends_on "qt5" => "with-d-bus"
   depends_on "gettext" => :build
 
+  def patches
+    DATA
+  end
+
   def install
     args = std_cmake_args
     args << "-DCMAKE_CXX_FLAGS='-D_DARWIN_C_SOURCE'"
@@ -21,3 +25,82 @@ class Kf5Kdesu < Formula
     prefix.install "install_manifest.txt"
   end
 end
+__END__
+From: Marko Käning <mk-lists@email.de>
+Date: Sat, 13 Dec 2014 14:00:15 +0000
+Subject: fix build on OSX
+X-Git-Url: http://quickgit.kde.org/?p=kdesu.git&a=commitdiff&h=163d9f1598596cfe8090f21c2562e25e64ad151b
+---
+fix build on OSX
+
+adds additional guard against using X11 on OSX
+
+CCMAIL: lukas@kde.org
+---
+
+
+--- a/src/kdesud/kdesud.cpp
++++ b/src/kdesud/kdesud.cpp
+@@ -73,7 +73,7 @@
+ #include "repo.h"
+ #include "handler.h"
+ 
+-#ifdef HAVE_X11
++#if defined(HAVE_X11) && !defined(Q_OS_MAC)
+ #include <X11/X.h>
+ #include <X11/Xlib.h>
+ #endif
+@@ -94,7 +94,7 @@
+ Repository *repo;
+ QString Version(QStringLiteral("1.01"));
+ QByteArray sock;
+-#ifdef HAVE_X11
++#if defined(HAVE_X11) && !defined(Q_OS_MAC)
+ Display *x11Display;
+ #endif
+ int pipeOfDeath[2];
+@@ -107,7 +107,7 @@
+ 
+ // Borrowed from kdebase/kaudio/kaudioserver.cpp
+ 
+-#ifdef HAVE_X11
++#if defined(HAVE_X11) && !defined(Q_OS_MAC)
+ extern "C" int xio_errhandler(Display *);
+ 
+ int xio_errhandler(Display *)
+@@ -307,7 +307,7 @@
+     if (pid)
+         _exit(0);
+ 
+-#ifdef HAVE_X11
++#if defined(HAVE_X11) && !defined(Q_OS_MAC)
+     // Make sure we exit when the display gets closed.
+     int x11Fd = initXconnection();
+     maxfd = qMax(maxfd, x11Fd);
+@@ -344,7 +344,7 @@
+     FD_ZERO(&active_fds);
+     FD_SET(sockfd, &active_fds);
+     FD_SET(pipeOfDeath[0], &active_fds);
+-#ifdef HAVE_X11
++#if defined(HAVE_X11) && !defined(Q_OS_MAC)
+     if (x11Fd != -1)
+         FD_SET(x11Fd, &active_fds);
+ #endif
+@@ -352,7 +352,7 @@
+     while (1)
+     {
+         tmp_fds = active_fds;
+-#ifdef HAVE_X11
++#if defined(HAVE_X11) && !defined(Q_OS_MAC)
+         if(x11Display)
+             XFlush(x11Display);
+ #endif
+@@ -396,7 +396,7 @@
+                 while(result > 0);
+             }
+ 
+-#ifdef HAVE_X11
++#if defined(HAVE_X11) && !defined(Q_OS_MAC)
+             if (i == x11Fd)
+             {
+                 // Discard X events
