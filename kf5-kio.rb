8a54d790e25bb1a9932a670a646887df22818a12
Nicolas Bigaouette <nbigaouette@gmail.com>
2015-02-11 13:16:53 -0500

    Disable X11 dependency requirement for Qt5.
    When having X11 installed on OSX, kf5-kio's CMakeLists.txt detects it and set the required
    dependence on Qt5 to include X11 support. Unfortunately, on OSX & homebrew, Qt5 is not
    compiled with X11 support as it should use native display.
    To fix, simply delete the detection of X11 for kio.
    
    See https://github.com/Homebrew/homebrew/issues/36660

--- kf5-kio.rb
+++ kf5-kio.rb
@@ -16,6 +16,10 @@ class Kf5Kio < Formula
   depends_on "haraldf/kf5/kf5-kwallet"
   depends_on "haraldf/kf5/kf5-solid"
 
+  def patches
+    DATA
+  end
+
   def install
     args = std_cmake_args
     args << "-DCMAKE_CXX_FLAGS='-D_DARWIN_C_SOURCE'"
@@ -27,3 +31,21 @@ class Kf5Kio < Formula
     prefix.install "install_manifest.txt"
   end
 end
+__END__
+diff --git a/CMakeLists.txt b/CMakeLists.txt
+index b72d6ee..20bf1c8 100644
+--- a/CMakeLists.txt
++++ b/CMakeLists.txt
+@@ -63,11 +63,6 @@
+ set_package_properties(GSSAPI PROPERTIES DESCRIPTION "Allows KIO to make use of
+                        TYPE OPTIONAL
+                        PURPOSE "A MIT or HEIMDAL flavor of GSSAPI can be used"
+                       )
+-find_package(X11)
+-set(HAVE_X11 ${X11_FOUND})
+-if (HAVE_X11)
+-    find_package(Qt5 ${REQUIRED_QT_VERSION} CONFIG REQUIRED X11Extras)
+-endif()
+
+ add_definitions(-DTRANSLATION_DOMAIN=\"kio5\")
+ if (IS_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}/po")
