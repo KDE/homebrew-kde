355ddc395a4882d024dde3f697966753d984bd2f
Harald Fernengel <harry@kdevelop.org>
2015-02-25 18:47:24 +0100

    Fix kf5-kdeclarative
    Closes #23

--- kf5-kdeclarative.rb
+++ kf5-kdeclarative.rb
@@ -10,7 +10,10 @@ class Kf5Kdeclarative < Formula
   depends_on "cmake" => :build
   depends_on "haraldf/kf5/kf5-extra-cmake-modules" => :build
   depends_on "qt5" => "with-d-bus"
+  depends_on "libepoxy"
   depends_on "haraldf/kf5/kf5-kio"
+
+  patch :DATA
 
   def install
     args = std_cmake_args
@@ -23,3 +26,18 @@ class Kf5Kdeclarative < Formula
     prefix.install "install_manifest.txt"
   end
 end
+
+__END__
+diff --git a/src/qmlcontrols/kquickcontrolsaddons/plotter.cpp b/src/qmlcontrols/kquickcontrolsaddons
+index 67ce63a..22934ad 100644
+--- a/src/qmlcontrols/kquickcontrolsaddons/plotter.cpp
++++ b/src/qmlcontrols/kquickcontrolsaddons/plotter.cpp
+@@ -47,6 +47,8 @@
+ 
+ #include <QuickAddons/ManagedTextureNode>
+ 
++#include <cmath>
++
+ //completely arbitrary
+ static int s_defaultSampleSize = 40;
+
