75017d38024614c12e2c31718542faedffeccbef
Nicolas Bigaouette <nbigaouette@gmail.com>
2015-04-17 14:32:49 -0400

    Dolphin in own repo; no need to patch anything.

--- kf5-dolphin.rb
+++ kf5-dolphin.rb
@@ -24,10 +24,6 @@ class Kf5Dolphin < Formula
   depends_on "haraldf/kf5/kf5-kparts"
   depends_on "haraldf/kf5/kf5-kdesu"
 
-  def patches
-    DATA
-  end
-
   def install
     args = std_cmake_args
 
@@ -35,47 +31,7 @@ class Kf5Dolphin < Formula
     args << "-DCMAKE_INSTALL_BUNDLEDIR=/Applications/KDE"
 
     system "cmake", ".", *args
-    cd "dolphin" do
-      system "make", "install"
-    end
-    cd "lib" do
-      system "make", "install"
-    end
+    system "make", "install"
   end
 end
-__END__
-diff --git a/CMakeLists.txt b/CMakeLists.txt
-index f925f60..e0f1d97 100644
---- a/CMakeLists.txt
-+++ b/CMakeLists.txt
-@@ -41,14 +41,14 @@ add_subdirectory(lib)
- add_subdirectory(kioslave/about)
- add_subdirectory(dolphin)
- add_subdirectory(kdialog)
--add_subdirectory(doc)
-+#add_subdirectory(doc)
- if(NOT WIN32)
-     add_subdirectory(kdepasswd)
- endif()
- add_subdirectory(kfind)
- 
--add_subdirectory( keditbookmarks )
--add_subdirectory(konqueror)
-+#add_subdirectory( keditbookmarks )
-+#add_subdirectory(konqueror)
- # add_subdirectory( konq-plugins )
- # if ( Q_WS_X11 )
- #   add_subdirectory( nsplugins )
-diff --git a/dolphin/CMakeLists.txt b/dolphin/CMakeLists.txt
-index 43e3807..c3c448f 100644
---- a/dolphin/CMakeLists.txt
-+++ b/dolphin/CMakeLists.txt
-@@ -55,7 +55,7 @@ if (KF5Baloo_FOUND AND KF5BalooWidgets_FOUND AND KF5FileMetaData_FOUND)
- endif()
- 
- add_subdirectory(src)
--add_subdirectory(docs)
-+#add_subdirectory(docs)
- 
- # TODO Remove the if/endif lines if Dolphin is split.
- if ("${CMAKE_SOURCE_DIR}" STREQUAL "${Dolphin_SOURCE_DIR}")
+
