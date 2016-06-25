651e86c7d9b48ac0c7942fa135f10c1aa0c16bf3
Ashish Bansal <bansal.ashish096@gmail.com>
2016-06-25 16:40:24 +0530

    Added formula for kmime

---
+++ kf5-kmime.rb
@@ -0,0 +1,18 @@
+require "formula"
+
+class Kf5Kmime < Formula
+  homepage "http://kde.org/"
+  head "git://anongit.kde.org/kmime.git"
+
+  depends_on "cmake" => :build
+  depends_on "haraldf/kf5/kf5-extra-cmake-modules" => :build
+  depends_on "haraldf/kf5/kf5-ki18n"
+  depends_on "haraldf/kf5/kf5-kcodecs"
+  depends_on "qt5" => "with-dbus"
+
+  def install
+    args = std_cmake_args
+    system "cmake", ".", *args
+    system "make", "install"
+  end
+end
