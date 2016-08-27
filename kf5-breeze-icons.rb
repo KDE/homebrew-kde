b3dc993cd2c4cb29d21cede118fd26082e28712a
Harald Fernengel <harry@kdevelop.org>
2016-08-27 22:29:55 +0200

    Use breeze icons instead of oxygen. Closes #59

--- kf5-breeze-icons.rb
+++ kf5-breeze-icons.rb
@@ -1,13 +1,14 @@
 require "formula"
 
-class Kf5OxygenIcons < Formula
+class Kf5BreezeIcons < Formula
   homepage "http://www.kde.org/"
-  url "http://download.kde.org/stable/applications/15.04.3/src/oxygen-icons-15.04.3.tar.xz"
-  sha256 "133b66d70b51641f841799674afd993a6af0d9d9b646021a0ed609beb2ffd71e"
+  url "http://download.kde.org/stable/frameworks/5.25/breeze-icons-5.25.0.tar.xz"
+  sha256 "e274c7f5c71c177e2e9d8ced9c49cd052ec2f726cb693c7df73e96af12277e8f"
 
-  head 'svn://anonsvn.kde.org/home/kde/trunk/kdesupport/oxygen-icons'
+  head 'git://anongit.kde.org/breeze-icons.git'
 
   depends_on "cmake" => :build
+  depends_on "qt5" => "with-dbus"
 
   def install
     args = std_cmake_args
@@ -15,12 +16,12 @@ class Kf5OxygenIcons < Formula
     system "cmake", ".", *args
     system "make", "install"
     prefix.install "install_manifest.txt"
-    ln_sf Dir["#{HOMEBREW_PREFIX}/share/icons"], "#{ENV['HOME']}/Library/Application Support/"
+    ln_sf Dir["#{HOMEBREW_PREFIX}/share/icons"], "#{Etc.getpwuid.dir}/Library/Application Support/"
   end
 
   def caveats; <<-EOS.undent
       A symlink "#{ENV['HOME']}/Library/Application Support/icons" was created
-      So that KF5 can find the oxygen themes.
+      So that KF5 can find the breeze themes.
 
       This symlink can be removed when this formula is uninstalled.
   EOS
