0675656ac6f28e54a245fc27d2a92f03b74d8d20
Roman Beránek <zavorka@users.noreply.github.com>
2016-10-02 18:23:34 +0200

    Add kparts dependency for kdelibs4support

--- kf5-kdelibs4support.rb
+++ kf5-kdelibs4support.rb
@@ -17,6 +17,7 @@ class Kf5Kdelibs4support < Formula
   depends_on "haraldf/kf5/kf5-kinit"
   depends_on "haraldf/kf5/kf5-kitemmodels"
   depends_on "haraldf/kf5/kf5-kunitconversion"
+  depends_on "haraldf/kf5/kf5-kparts"
 
   def install
     args = std_cmake_args
