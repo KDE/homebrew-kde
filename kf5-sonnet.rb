db1ad5bb3d8b485b116791bc724ae7f26e09c15f
Harald Fernengel <harry@kdevelop.org>
2017-05-24 20:44:34 +0300

    Set QT_NO_EXCEPTIONS when building *.mm files
    Apparently, exceptions are disabled when building *.mm files.
    The alternative would be to set
    
    target_compile_options(sonnet_nsspellchecker PRIVATE "-fexceptions")
    
    in order to force exception to be on for *.mm files. However, since
    exceptions aren't used in the code, let's just build without.
    
    Closes #99

--- kf5-sonnet.rb
+++ kf5-sonnet.rb
@@ -11,6 +11,8 @@ class Kf5Sonnet < Formula
   depends_on "haraldf/kf5/kf5-extra-cmake-modules" => :build
   depends_on "qt5"
 
+  patch :DATA
+
   def install
     args = std_cmake_args
 
@@ -19,3 +21,18 @@ class Kf5Sonnet < Formula
     prefix.install "install_manifest.txt"
   end
 end
+
+__END__
+diff --git a/src/plugins/nsspellchecker/CMakeLists.txt b/src/plugins/nsspellchecker/CMakeLists.txt
+index f8c99d1..f2fa4a9 100644
+--- a/src/plugins/nsspellchecker/CMakeLists.txt
++++ b/src/plugins/nsspellchecker/CMakeLists.txt
+@@ -7,6 +7,8 @@ add_library(sonnet_nsspellchecker MODULE ${sonnet_nsspellchecker_PART_SRCS})
+ 
+ target_link_libraries(sonnet_nsspellchecker PRIVATE KF5::SonnetCore "-framework Cocoa")
+ 
++target_compile_definitions(sonnet_nsspellchecker PRIVATE "QT_NO_EXCEPTIONS")
++
+ set_target_properties(sonnet_nsspellchecker PROPERTIES OUTPUT_NAME "nsspellchecker")
+ install(TARGETS sonnet_nsspellchecker  DESTINATION ${KDE_INSTALL_PLUGINDIR}/kf5/sonnet/)
+ 
