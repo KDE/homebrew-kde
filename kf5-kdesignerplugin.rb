29fd5c8daae1e456837caa14de6abfc9d0f73e9a
Harald Fernengel <harry@kdevelop.org>
2017-05-26 21:36:59 +0300

    kdesignerplugins depends on core-addons
    Closes #115

--- kf5-kdesignerplugin.rb
+++ kf5-kdesignerplugin.rb
@@ -10,6 +10,7 @@ class Kf5Kdesignerplugin < Formula
   depends_on "cmake" => :build
   depends_on "gettext" => :build
   depends_on "haraldf/kf5/kf5-extra-cmake-modules" => :build
+  depends_on "haraldf/kf5/kf5-kcoreaddons"
   depends_on "haraldf/kf5/kf5-kdoctools" => :build
   #  depends_on "haraldf/kf5/kf5-kdewebkit"
   depends_on "haraldf/kf5/kf5-kplotting"
