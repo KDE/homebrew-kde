96b54de4eca2eb32ab788a38ec2a8f8a66601cf2
Yurii Kolesnykov <root@yurikoles.com>
2017-08-10 00:30:46 +0300

    cleanup clazy

--- clazy.rb
+++ clazy.rb
@@ -1,8 +1,10 @@
 class Clazy < Formula
-  desc "Qt oriented code checker based on clang framework. Krazy's little brother."
-  homepage ""
-  url "https://github.com/KDE/clazy/archive/v1.2.tar.gz"
-  sha256 "e4177a1a0e1d03577714fd95091e2af2f9ec0bda6cab1d1eab4f3232f53afb7e"
+  desc "Qt oriented code checker based on clang framework."
+  homepage "https://github.com/KDE/clazy"
+  url "https://download.kde.org/stable/clazy/1.2/src/clazy-1.2.tar.xz"
+  sha256 "c68fed3d719c23ffefeedd88a02984bf37dba6a55dad6359786909a9e5778f91"
+
+  head "git://anongit.kde.org/clazy"
 
   depends_on "cmake" => :build
   depends_on "llvm"
@@ -10,9 +12,9 @@ class Clazy < Formula
 
   def install
     args = std_cmake_args
-
+    args << "-DCMAKE_INSTALL_MANDIR=#{man}"
     system "cmake", ".", *args
     system "make", "install"
     prefix.install "install_manifest.txt"
- end 
+  end
 end
