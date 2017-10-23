0c827589ad4857255b94b1191501dbb1a4cb36ff
Gustavo Alvarez <sl1pkn07@gmail.com>
2017-10-23 22:58:50 +0200

    Fix kdelibs4support installation (https://github.com/haraldF/homebrew-kf5/issues/133 related) - Also include patch for support openssl 1.1 branch (openssl@1.1). this also fix https://github.com/haraldF/homebrew-kf5/issues/51   (now search the openssl 1.1 libs and includes)

--- kf5-kdelibs4support.rb
+++ kf5-kdelibs4support.rb
@@ -19,14 +19,31 @@ class Kf5Kdelibs4support < Formula
   depends_on "haraldf/kf5/kf5-kunitconversion"
   depends_on "haraldf/kf5/kf5-kparts"
   depends_on "qt"
+  depends_on "openssl@1.1"
+
+  patch do
+    url "https://git.archlinux.org/svntogit/packages.git/plain/trunk/kdelibs4support-openssl-1.1.patch?h=packages/kdelibs4support"
+    sha256  "e33de96fae2b93c3fa06b4219205ae188b214be8c5e84b8d0426217fd65a5d48"
+  end
 
   def install
     args = std_cmake_args
+    args << "-DCMAKE_INSTALL_BUNDLEDIR=#{prefix}/bin"
+    args << "-DOPENSSL_CRYPTO_LIBRARY=/usr/local/opt/openssl@1.1/lib/libcrypto.dylib"
+    args << "-DOPENSSL_SSL_LIBRARY=/usr/local/opt/openssl@1.1/lib/libssl.1.1.dylib"
+    args << "-DOPENSSL_INCLUDE_DIR=/usr/local/opt/openssl@1.1/"
 
     mkdir "build" do
       system "cmake", "..", *args
       system "make", "install"
       prefix.install "install_manifest.txt"
     end
+  end
+
+  def caveats; <<-EOS.undent
+    You need to take some manual steps in order to make this formula work:
+      mkdir -p "~/Applications/KDE"
+      ln -sf "#{prefix}/bin/kdebugdialog5.app" "~/Applications/KDE/"
+    EOS
   end
 end
