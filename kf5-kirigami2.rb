fbaa34e2a02fa7e0f147bfb6f1bff611cfbda56d
Gustavo Alvarez <sl1pkn07@gmail.com>
2017-11-02 01:09:06 +0100

    Add kf5-kirigami2 and kf5-qqc2-desktop-style frameworks

---
+++ kf5-kirigami2.rb
@@ -0,0 +1,27 @@
+require "formula"
+
+class Kf5Kirigami2 < Formula
+  desc "A QtQuick based components set"
+  homepage "http://www.kde.org/"
+  url "https://download.kde.org/stable/frameworks/5.39/kirigami2-5.39.0.tar.xz"
+  sha256 "2714ee82dbcc2cd1b799d5f12193cae5637136466295567426c74b4f26f5ef6a"
+
+  head "git://anongit.kde.org/kirigami2.git"
+
+  depends_on "cmake" => :build
+  depends_on "doxygen" => :build
+  depends_on "KDE-mac/kde/kf5-extra-cmake-modules" => :build
+
+  depends_on "qt"
+
+  def install
+    args = std_cmake_args
+    args << "-DBUILD_TESTING=OFF"
+
+    mkdir "build" do
+      system "cmake", "..", *args
+      system "make", "install"
+      prefix.install "install_manifest.txt"
+    end
+  end
+end
---
+++ kf5-qqc2-desktop-style.rb
@@ -0,0 +1,29 @@
+require "formula"
+
+class Kf5Qqc2DesktopStyle < Formula
+  desc "A style for Qt Quick Controls 2 to make it follow your desktop theme"
+  homepage "http://www.kde.org/"
+  url "https://download.kde.org/stable/frameworks/5.39/qqc2-desktop-style-5.39.0.tar.xz"
+  sha256 "fb8818b22dbc1f47c38bef488543f025ec5da810e84f5ed732b08969338411e1"
+
+  head "git://anongit.kde.org/qqc2-desktop-style.git"
+
+  depends_on "cmake" => :build
+  depends_on "KDE-mac/kde/kf5-extra-cmake-modules" => :build
+
+  depends_on "qt"
+  depends_on "KDE-mac/kde/kf5-kirigami2" => :build
+  depends_on "KDE-mac/kde/kf5-kiconthemes" => :build
+  depends_on "KDE-mac/kde/kf5-kconfigwidgets" => :build
+
+  def install
+    args = std_cmake_args
+    args << "-DBUILD_TESTING=OFF"
+
+    mkdir "build" do
+      system "cmake", "..", *args
+      system "make", "install"
+      prefix.install "install_manifest.txt"
+    end
+  end
+end
