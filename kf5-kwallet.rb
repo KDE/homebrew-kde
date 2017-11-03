2bf07fe7b9d74d8fe3621bfd5ae363e963716393
Gustavo Alvarez <sl1pkn07@gmail.com>
2017-11-03 18:39:45 +0100

    kf5-kwallet: add missing gettext

--- kf5-kwallet.rb
+++ kf5-kwallet.rb
@@ -12,6 +12,7 @@ class Kf5Kwallet < Formula
   depends_on "boost" => :build
   depends_on "doxygen" => :build
   depends_on "graphviz" => :build
+  depends_on "gettext" => :build
   depends_on "KDE-mac/kde/kf5-extra-cmake-modules" => :build
   depends_on "KDE-mac/kde/kf5-kdoctools" => :build
 
