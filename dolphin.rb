ded31d9495186c1f87c9f3dab81db45d01ddc985
Gustavo Alvarez <sl1pkn07@gmail.com>
2017-11-12 16:43:17 +0100

    Revert "Add kio-extras formulae (also update deps&ops for dolphin and kf5-kio)"
    This reverts commit 2605f3a6b49fb2afc33bfb2c5b53e8ff6d9d3525.

--- dolphin.rb
+++ dolphin.rb
@@ -3,7 +3,6 @@ require "formula"
 class Dolphin < Formula
   url "https://download.kde.org/stable/applications/17.08.3/src/dolphin-17.08.3.tar.xz"
   sha256 "651565291d06faa60fc4baf3b11aa4ba766da8b654b1c8891ec0cb1088316b09"
-  revision 2
   desc "KDE File Manager"
   homepage "http://www.kde.org/"
 
@@ -11,7 +10,6 @@ class Dolphin < Formula
 
   depends_on "cmake" => :build
   depends_on "KDE-mac/kde/kf5-extra-cmake-modules" => :build
-  depends_on "KDE-mac/kde/kf5-kdoctools" => :build
 
   depends_on "ruby" => :optional
   depends_on "KDE-mac/kde/konsole" => :optional
@@ -22,7 +20,6 @@ class Dolphin < Formula
   depends_on "KDE-mac/kde/kf5-kparts"
   depends_on "KDE-mac/kde/kf5-kinit"
   depends_on "KDE-mac/kde/kf5-kcoreaddons"
-  depends_on "KDE-mac/kde/kio-extras"
 
   def install
     args = std_cmake_args
--- kf5-kio.rb
+++ kf5-kio.rb
@@ -3,28 +3,28 @@ require "formula"
 class Kf5Kio < Formula
   url "https://download.kde.org/stable/frameworks/5.40/kio-5.40.0.tar.xz"
   sha256 "30ea0b231b995faaf4283b9c9ecfaffb589268f5d7b5b805f69ed95601ac389b"
-  revision 2
   desc "Resource and network access abstraction"
   homepage "http://www.kde.org/"
 
   head "git://anongit.kde.org/kio.git"
+
+  option "without-kiowidgets", "Builds only KIO core"
 
   depends_on "cmake" => :build
   depends_on "doxygen" => :build
   depends_on "graphviz" => :build
   depends_on "KDE-mac/kde/kf5-extra-cmake-modules" => :build
 
-  depends_on "KDE-mac/kde/kf5-kdoctools" => :optional
-  depends_on "KDE-mac/kde/kio-extras" => :optional
-
   depends_on "qt"
   depends_on "libxslt"
+  depends_on "libxml2"
   depends_on "KDE-mac/kde/kf5-karchive"
   depends_on "KDE-mac/kde/kf5-solid"
   depends_on "KDE-mac/kde/kf5-kservice"
-  depends_on "KDE-mac/kde/kf5-kbookmarks"
-  depends_on "KDE-mac/kde/kf5-kjobwidgets"
-  depends_on "KDE-mac/kde/kf5-kwallet"
+  depends_on "KDE-mac/kde/kf5-kdoctools"
+  depends_on "KDE-mac/kde/kf5-kbookmarks" unless build.without? "kiowidgets"
+  depends_on "KDE-mac/kde/kf5-kjobwidgets" unless build.without? "kiowidgets"
+  depends_on "KDE-mac/kde/kf5-kwallet" unless build.without? "kiowidgets"
 
   def install
     args = std_cmake_args
