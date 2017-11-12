61ad19f8dba7baae53dbfb9ffed122332c753a5d
Yurii Kolesnykov <root@yurikoles.com>
2017-11-12 18:20:39 +0200

    kf5-ki18n: make doxygen build-dep

--- kf5-ki18n.rb
+++ kf5-ki18n.rb
@@ -11,7 +11,7 @@ class Kf5Ki18n < Formula
   depends_on "cmake" => :build
   depends_on "gettext" => :build
   depends_on "KDE-mac/kde/kf5-extra-cmake-modules" => :build
-  depends_on "doxygen"
+  depends_on "doxygen" => :build
 
   depends_on "qt"
 
