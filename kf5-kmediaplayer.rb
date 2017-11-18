a12b03dd4e4e85e4f1c2d06de18b52425887b0b6
Gustavo Alvarez <sl1pkn07@gmail.com>
2017-11-18 17:52:56 +0100

    kf5-mediaplayer: fix HEAD url

--- kf5-kmediaplayer.rb
+++ kf5-kmediaplayer.rb
@@ -4,7 +4,7 @@ class Kf5Kmediaplayer < Formula
   url "https://download.kde.org/stable/frameworks/5.40/portingAids/kmediaplayer-5.40.0.tar.xz"
   sha256 "f44e170996411fcde7a63fd847631a4b8ed45d7098a56a26b866b3fd0d9bfe8e"
 
-  head "git://anongit.kde.org/attica.git"
+  head "git://anongit.kde.org/kmediaplayer.git"
 
   depends_on "cmake" => :build
   depends_on "KDE-mac/kde/kf5-extra-cmake-modules" => :build
