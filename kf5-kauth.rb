ac81ad5d832ce866f6f0bdd51c0fadc0d82659cb
Sergei Reznikov <diver@gelios.net>
2018-01-21 19:13:06 +0300

    kauth: fix build (#199)
    * kauth: fix build
    
    * Update kf5-kauth.rb

--- kf5-kauth.rb
+++ kf5-kauth.rb
@@ -3,7 +3,7 @@ class Kf5Kauth < Formula
   homepage "https://www.kde.org"
   url "https://download.kde.org/stable/frameworks/5.42/kauth-5.42.0.tar.xz"
   sha256 "91ebf3554551c3815e89e53e577e42d7cc3ac1f4966215fbbc93a60ca1587812"
-  revision 1
+  revision 2
 
   head "git://anongit.kde.org/kauth.git"
 
@@ -14,6 +14,8 @@ class Kf5Kauth < Formula
 
   depends_on "gpgme"
   depends_on "KDE-mac/kde/kf5-kcoreaddons"
+
+  patch :DATA
 
   def install
     args = std_cmake_args
@@ -35,3 +37,47 @@ class Kf5Kauth < Formula
      EOS
   end
 end
+
+__END__
+
+diff --git a/src/backends/fake/kauth-policy-gen-polkit.cpp b/src/backends/fake/kauth-policy-gen-polkit.cpp
+index d103414..5575620 100644
+--- a/src/backends/fake/kauth-policy-gen-polkit.cpp
++++ b/src/backends/fake/kauth-policy-gen-polkit.cpp
+@@ -38,7 +38,7 @@ const char policy_tag[] = ""
+ 
+ const char dent[] = "   ";
+ 
+-void output(QList<Action> actions, QMap<QString, QString> domain)
++void output(QList<Action> actions, const QMap<QString, QString> &domain)
+ {
+     Q_UNUSED(domain)
+ 
+diff --git a/src/backends/mac/kauth-policy-gen-mac.cpp b/src/backends/mac/kauth-policy-gen-mac.cpp
+index e146c46..ccfc0e7 100644
+--- a/src/backends/mac/kauth-policy-gen-mac.cpp
++++ b/src/backends/mac/kauth-policy-gen-mac.cpp
+@@ -26,7 +26,7 @@
+ 
+ using namespace std;
+ 
+-void output(QList<Action> actions, QMap<QString, QString> domain)
++void output(QList<Action> actions, const QMap<QString, QString> &domain)
+ {
+     AuthorizationRef auth;
+     AuthorizationCreate(NULL, kAuthorizationEmptyEnvironment, kAuthorizationFlagDefaults, &auth);
+diff --git a/src/backends/policykit/kauth-policy-gen-polkit.cpp b/src/backends/policykit/kauth-policy-gen-polkit.cpp
+index 4f29cb0..c215275 100644
+--- a/src/backends/policykit/kauth-policy-gen-polkit.cpp
++++ b/src/backends/policykit/kauth-policy-gen-polkit.cpp
+@@ -39,7 +39,7 @@ const char policy_tag[] = ""
+ 
+ const char dent[] = "   ";
+ 
+-void output(QList<Action> actions, QHash<QString, QString> domain)
++void output(QList<Action> actions, const QMap<QString, QString> &domain)
+ {
+     QTextStream out(stdout);
+     out.setCodec("UTF-8");
+
+
