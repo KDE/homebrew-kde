d0ea586e970dcea8967e2a95883823649f06633a
Hessel van der Molen <hmolen.science@gmail.com>
2018-03-13 11:04:02 +0100

    kf5-kdnssd.rb : patching DNSServiceResolve

--- Formula/kf5-kdnssd.rb
+++ Formula/kf5-kdnssd.rb
@@ -13,6 +13,8 @@ class Kf5Kdnssd < Formula
 
   depends_on "qt"
 
+  patch :DATA
+
   def install
     args = std_cmake_args
     args << "-DBUILD_TESTING=OFF"
@@ -27,3 +29,57 @@ class Kf5Kdnssd < Formula
     end
   end
 end
+
+__END__
+
+diff --git a/src/mdnsd-publicservice.cpp b/src/mdnsd-publicservice.cpp
+index 7f39ef6..7451d71 100644
+--- a/src/mdnsd-publicservice.cpp
++++ b/src/mdnsd-publicservice.cpp
+@@ -160,7 +160,7 @@ void PublicService::publishAsync()
+     TXTRecordCreate(&txt, 0, 0);
+     QMap<QString, QByteArray>::ConstIterator itEnd = d->m_textData.cend();
+     for (QMap<QString, QByteArray>::ConstIterator it = d->m_textData.cbegin(); it != itEnd; ++it) {
+-        if (TXTRecordSetValue(&txt, it.key().toUtf8(), it.value().length(), it.value()) != kDNSServiceErr_NoError) {
++        if (TXTRecordSetValue(&txt, it.key().toUtf8().constData(), it.value().length(), it.value().constData()) != kDNSServiceErr_NoError) {
+             TXTRecordDeallocate(&txt);
+             emit published(false);
+             return;
+@@ -171,7 +171,7 @@ void PublicService::publishAsync()
+     Q_FOREACH (const QString &subtype, d->m_subtypes) {
+         fullType += ',' + subtype;
+     }
+-    if (DNSServiceRegister(&ref, 0, 0, d->m_serviceName.toUtf8(), fullType.toLatin1().constData(), domainToDNS(d->m_domain), NULL,
++    if (DNSServiceRegister(&ref, 0, 0, d->m_serviceName.toUtf8().constData(), fullType.toLatin1().constData(), domainToDNS(d->m_domain).constData(), NULL,
+                            htons(d->m_port), TXTRecordGetLength(&txt), TXTRecordGetBytesPtr(&txt), publish_callback,
+                            reinterpret_cast<void *>(d)) == kDNSServiceErr_NoError) {
+         d->setRef(ref);
+diff --git a/src/mdnsd-remoteservice.cpp b/src/mdnsd-remoteservice.cpp
+index d15725b..88f75f3 100644
+--- a/src/mdnsd-remoteservice.cpp
++++ b/src/mdnsd-remoteservice.cpp
+@@ -82,8 +82,8 @@ void RemoteService::resolveAsync()
+     d->m_resolved = false;
+     //qDebug() << this << ":Starting resolve of : " << d->m_serviceName << " " << d->m_type << " " << d->m_domain << "\n";
+     DNSServiceRef ref;
+-    if (DNSServiceResolve(&ref, 0, 0, d->m_serviceName.toUtf8(), d->m_type.toLatin1().constData(),
+-                          domainToDNS(d->m_domain), (DNSServiceResolveReply)resolve_callback, reinterpret_cast<void *>(d))
++    if (DNSServiceResolve(&ref, 0, 0, d->m_serviceName.toUtf8().constData(), d->m_type.toLatin1().constData(),
++                          domainToDNS(d->m_domain).constData(), (DNSServiceResolveReply)resolve_callback, reinterpret_cast<void *>(d))
+             == kDNSServiceErr_NoError) {
+         d->setRef(ref);
+     }
+diff --git a/src/mdnsd-servicebrowser.cpp b/src/mdnsd-servicebrowser.cpp
+index 476bb17..41ec51d 100644
+--- a/src/mdnsd-servicebrowser.cpp
++++ b/src/mdnsd-servicebrowser.cpp
+@@ -100,7 +100,7 @@ void ServiceBrowser::startBrowse()
+         fullType = d->m_subtype + "._sub." + d->m_type;
+     }
+     if (DNSServiceBrowse(&ref, 0, 0, fullType.toLatin1().constData(),
+-                         domainToDNS(d->m_domain), query_callback, reinterpret_cast<void *>(d))
++                         domainToDNS(d->m_domain).constData(), query_callback, reinterpret_cast<void *>(d))
+             == kDNSServiceErr_NoError) {
+         d->setRef(ref);
+     }
+
