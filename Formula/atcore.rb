a4e8c7a4616db5e89fd04f035bd1aae2fcd4a92e
Chris Rizzitello <sithlord48@gmail.com>
2018-05-08 23:13:52 -0400

    Add Formula for AtCore

---
+++ Formula/atcore.rb
@@ -0,0 +1,25 @@
+class Atcore < Formula
+  desc "AtCore is a API to manage the connection to 3D Printers"
+  homepage "https://www.kde.org"
+  url "https://download.kde.org/stable/atcore/1.0.0/atcore-1.0.0.tar.xz"
+  sha256 "ffd12455c9b8db853e455a437d6c6b601e0003c6732bbc6c2828032e004530e2"
+
+  head "git://anongit.kde.org/atcore.git"
+
+  depends_on "cmake" => :build
+  depends_on "KDE-mac/kde/kf5-extra-cmake-modules" => :build
+
+  depends_on "qt"
+
+  def install
+    args = std_cmake_args
+    args << "-DBUILD_TESTGUI=ON"
+    args << "-DKDE_INSTALL_PLUGINDIR=lib/qt5/plugins"
+
+    mkdir "build" do
+      system "cmake", "..", *args
+      system "make", "install"
+      prefix.install "install_manifest.txt"
+    end
+  end
+end
