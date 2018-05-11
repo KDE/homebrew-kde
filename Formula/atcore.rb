6c71127f10a78d917de68e1bb67930715ed18f35
Yurii Kolesnykov <root@yurikoles.com>
2018-05-12 01:26:43 +0300

    fix audit warnings

--- Formula/atcore.rb
+++ Formula/atcore.rb
@@ -1,14 +1,13 @@
 class Atcore < Formula
-  desc "AtCore is a API to manage the connection to 3D Printers"
+  desc "API to manage the connection to 3D Printers"
   homepage "https://www.kde.org"
   url "https://download.kde.org/stable/atcore/1.0.0/atcore-1.0.0.tar.xz"
   sha256 "ffd12455c9b8db853e455a437d6c6b601e0003c6732bbc6c2828032e004530e2"
 
   head "git://anongit.kde.org/atcore.git"
 
-  depends_on "cmake" => :build
   depends_on "KDE-mac/kde/kf5-extra-cmake-modules" => :build
-
+  depends_on "cmake" => :build
   depends_on "qt"
 
   def install
