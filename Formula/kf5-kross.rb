f8db0266d3e4239306e336f40097cc17d83eb40d
Yurii Kolesnykov <root@yurikoles.com>
2018-07-08 01:14:02 +0300

    new formula: kf5-kross

--- Formula/kf5-kross.rb
+++ Formula/kf5-kross.rb
@@ -1,6 +1,6 @@
 class Kf5Kross < Formula
-  desc "Multi-language application scripting"
-  homepage "https://www.kde.org"
+  desc "Embedding of scripting into applications"
+  homepage "https://api.kde.org/frameworks/kross/html"
   url "https://download.kde.org/stable/frameworks/5.47/portingAids/kross-5.47.0.tar.xz"
   sha256 "01af097ef87d37ca21ce87b4ee6b67555f2c1051167d1e6b553748181ddaca8e"
 
@@ -8,17 +8,25 @@ class Kf5Kross < Formula
 
   depends_on "cmake" => :build
   depends_on "KDE-mac/kde/kf5-extra-cmake-modules" => :build
-  depends_on "KDE-mac/kde/kf5-kdoctools" => :build
-
+  depends_on "KDE-mac/kde/kf5-kxmlgui" => :build
+  depends_on "gettext"
+  depends_on "KDE-mac/kde/kf5-kcompletion"
+  depends_on "KDE-mac/kde/kf5-kcoreaddons"
+  depends_on "KDE-mac/kde/kf5-ki18n"
+  depends_on "KDE-mac/kde/kf5-kiconthemes"
+  depends_on "KDE-mac/kde/kf5-kio"
   depends_on "KDE-mac/kde/kf5-kparts"
-
-  patch :DATA
+  depends_on "KDE-mac/kde/kf5-kwidgetsaddons"
+  depends_on "qt"
+  depends_on "KDE-mac/kde/kf5-kdoctools" => :optional
 
   def install
     args = std_cmake_args
     args << "-DBUILD_TESTING=OFF"
+    args << "-DBUILD_QCH=ON"
     args << "-DKDE_INSTALL_QMLDIR=lib/qt5/qml"
     args << "-DKDE_INSTALL_PLUGINDIR=lib/qt5/plugins"
+    args << "-DCMAKE_INSTALL_BUNDLEDIR=#{bin}"
 
     mkdir "build" do
       system "cmake", "..", *args
@@ -26,52 +34,15 @@ class Kf5Kross < Formula
       prefix.install "install_manifest.txt"
     end
   end
+
+  def caveats; <<~EOS
+    You need to take some manual steps in order to make this formula work:
+      mkdir -p $HOME/Applications/KDE
+      ln -sf "$(brew --prefix)/opt/kf5-kross/bin/kf5kross.app" $HOME/Applications/KDE/
+  EOS
+  end
+
+  test do
+    assert `$(brew --prefix)/bin/kate.app/Contents/MacOS/kate --help | grep -- --help` =~ /--help/
+  end
 end
-
-# Make nongui kf5kross
-# Fix typo(?) in qts-interpreter cmakelist.txt
-__END__
-diff --git a/CMakeLists.txt b/CMakeLists.txt
-index 1bf53a0..45ad3d3 100644
---- a/CMakeLists.txt
-+++ b/CMakeLists.txt
-@@ -20,6 +20,8 @@ include(ECMGenerateHeaders)
- 
- include(ECMQtDeclareLoggingCategory)
- 
-+include(ECMMarkNonGuiExecutable)
-+
- include(KDEInstallDirs)
- include(KDEFrameworkCompilerSettings NO_POLICY_SCOPE)
- include(KDECMakeSettings)
-diff --git a/src/console/CMakeLists.txt b/src/console/CMakeLists.txt
-index 8e15a1a..6808f52 100644
---- a/src/console/CMakeLists.txt
-+++ b/src/console/CMakeLists.txt
-@@ -7,4 +7,5 @@ target_link_libraries(kf5kross
-    Qt5::Widgets
- )
- 
-+ecm_mark_nongui_executable(kf5kross)
- install(TARGETS kf5kross ${KF5_INSTALL_TARGETS_DEFAULT_ARGS})
-
-diff --git a/src/qts-interpreter/CMakeLists.txt b/src/qts-interpreter/CMakeLists.txt
-index 7836610..c34ce42 100644
---- a/src/qts-interpreter/CMakeLists.txt
-+++ b/src/qts-interpreter/CMakeLists.txt
-@@ -12,4 +12,4 @@ target_link_libraries(krossqts
-     Qt5::Core
- )
- 
--install(TARGETS krossqts DESTINATION ${KDE_INSTALL_QTPLUGINDIR})
-+install(TARGETS krossqts DESTINATION ${KDE_INSTALL_PLUGINDIR})
-diff --git a/src/qts/CMakeLists.txt b/src/qts/CMakeLists.txt
-index 93d3bd9..d21e6d6 100644
---- a/src/qts/CMakeLists.txt
-+++ b/src/qts/CMakeLists.txt
-@@ -16,4 +16,4 @@ target_link_libraries(krossqtsplugin
-     Qt5::Core
- )
- 
--install(TARGETS krossqtsplugin DESTINATION ${KDE_INSTALL_QTPLUGINDIR}/script)
-+install(TARGETS krossqtsplugin DESTINATION ${KDE_INSTALL_PLUGINDIR}/script)
