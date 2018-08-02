KDE/homebrew-kde

commit 7cc286433414c3480c23416f9867a5210886d4fe

Update:  * frameworks 5.47  * applications 18.04.2

Formula/kf5-tier1-frameworks.rb
@@ -1,7 +1,6 @@
 class Kf5Tier1Frameworks < Formula
   desc "Metapackage for Tier 1 KF5 frameworks"
   homepage "https://api.kde.org/frameworks"
-  system "touch", "/tmp/empty"
   url "file:///tmp/empty"
   version "5.45.0"
   sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"

Formula/kf5-tier2-frameworks.rb
@@ -1,7 +1,6 @@
 class Kf5Tier2Frameworks < Formula
   desc "Metapackage for Tier 2 KF5 frameworks"
   homepage "https://api.kde.org/frameworks"
-  system "touch", "/tmp/empty"
   url "file:///tmp/empty"
   version "5.45.0"
   sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"

Formula/kf5-tier3-frameworks.rb
@@ -1,7 +1,6 @@
 class Kf5Tier3Frameworks < Formula
   desc "Metapackage for Tier 3 KF5 frameworks"
   homepage "https://api.kde.org/frameworks"
-  system "touch", "/tmp/empty"
   url "file:///tmp/empty"
   version "5.45.0"
   sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"

Formula/kf5-tier4-frameworks.rb
@@ -1,7 +1,6 @@
 class Kf5Tier4Frameworks < Formula
   desc "Metapackage for Tier 4 KF5 frameworks"
   homepage "https://api.kde.org/frameworks"
-  system "touch", "/tmp/empty"
   url "file:///tmp/empty"
   version "5.45.0"
   sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"

commit e8359cf24816617ffec692fe94574930d5a7a26c

fix kf5-tier#-frameworks.rb regression introduced in 7cc2864 closes #248

Formula/kf5-tier1-frameworks.rb
@@ -1,6 +1,7 @@
 class Kf5Tier1Frameworks < Formula
   desc "Metapackage for Tier 1 KF5 frameworks"
   homepage "https://api.kde.org/frameworks"
+  system "touch", "/tmp/empty"
   url "file:///tmp/empty"
   version "5.45.0"
   sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"

Formula/kf5-tier2-frameworks.rb
@@ -1,6 +1,7 @@
 class Kf5Tier2Frameworks < Formula
   desc "Metapackage for Tier 2 KF5 frameworks"
   homepage "https://api.kde.org/frameworks"
+  system "touch", "/tmp/empty"
   url "file:///tmp/empty"
   version "5.45.0"
   sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"

Formula/kf5-tier3-frameworks.rb
@@ -1,6 +1,7 @@
 class Kf5Tier3Frameworks < Formula
   desc "Metapackage for Tier 3 KF5 frameworks"
   homepage "https://api.kde.org/frameworks"
+  system "touch", "/tmp/empty"
   url "file:///tmp/empty"
   version "5.45.0"
   sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"

Formula/kf5-tier4-frameworks.rb
@@ -1,6 +1,7 @@
 class Kf5Tier4Frameworks < Formula
   desc "Metapackage for Tier 4 KF5 frameworks"
   homepage "https://api.kde.org/frameworks"
+  system "touch", "/tmp/empty"
   url "file:///tmp/empty"
   version "5.45.0"
   sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
e8359cf24816617ffec692fe94574930d5a7a26c
Yurii Kolesnykov <root@yurikoles.com>
2018-08-02 19:22:54 +0300

    fix kf5-tier#-frameworks.rb regression introduced in 7cc2864 closes #248

--- Formula/kf5-tier4-frameworks.rb
+++ Formula/kf5-tier4-frameworks.rb
@@ -1,6 +1,7 @@
 class Kf5Tier4Frameworks < Formula
   desc "Metapackage for Tier 4 KF5 frameworks"
   homepage "https://api.kde.org/frameworks"
+  system "touch", "/tmp/empty"
   url "file:///tmp/empty"
   version "5.45.0"
   sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
