f5d1c587a23ca7521747753aa2c8c8b907820656
Darcy Shen <sadhen@zoho.com>
2018-11-20 01:07:36 +0800

    A Retina Okular

--- Formula/okular.rb
+++ Formula/okular.rb
@@ -71,15 +71,88 @@ end
 
 __END__
 diff --git a/CMakeLists.txt b/CMakeLists.txt
-index d69765e6c..70d459684 100644
+index 579379dc8..cd59e37f3 100644
 --- a/CMakeLists.txt
 +++ b/CMakeLists.txt
 @@ -261,7 +261,7 @@ endif()
- 
+
  # Special handling for linking okularcore on OSX/Apple
  IF(APPLE)
 -    SET(OKULAR_IOKIT "-framework IOKit" CACHE STRING "Apple IOKit framework")
 +    SET(OKULAR_IOKIT "-framework CoreFoundation -framework CoreGraphics -framework IOKit" CACHE STRING "Apple IOKit framework")
  ENDIF(APPLE)
- 
+
  # Extra library needed by imported synctex code on Windows
+diff --git a/shell/CMakeLists.txt b/shell/CMakeLists.txt
+index 628f74be1..f4e807dc5 100644
+--- a/shell/CMakeLists.txt
++++ b/shell/CMakeLists.txt
+@@ -25,6 +25,7 @@ if(TARGET KF5::Activities)
+ 	target_link_libraries(okular KF5::Activities)
+ endif()
+
++set_target_properties(okular PROPERTIES MACOSX_BUNDLE_INFO_PLIST ${CMAKE_CURRENT_SOURCE_DIR}/MacOSXBundleInfo.plist.in)
+ install(TARGETS okular ${KDE_INSTALL_TARGETS_DEFAULT_ARGS})
+
+
+diff --git a/shell/MacOSXBundleInfo.plist.in b/shell/MacOSXBundleInfo.plist.in
+new file mode 100644
+index 000000000..ab421eaf8
+--- /dev/null
++++ b/shell/MacOSXBundleInfo.plist.in
+@@ -0,0 +1,55 @@
++<?xml version="1.0" encoding="UTF-8"?>
++<!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
++<plist version="1.0">
++<dict>
++        <key>NSPrincipalClass</key>
++        <string>NSApplication</string>
++        <key>NSHighResolutionCapable</key>
++        <string>True</string>
++        <key>CFBundleDevelopmentRegion</key>
++        <string>English</string>
++        <key>CFBundleExecutable</key>
++        <string>${MACOSX_BUNDLE_EXECUTABLE_NAME}</string>
++        <key>CFBundleGetInfoString</key>
++        <string>${MACOSX_BUNDLE_INFO_STRING}</string>
++        <key>CFBundleIconFile</key>
++        <string>${MACOSX_BUNDLE_ICON_FILE}</string>
++        <key>CFBundleIdentifier</key>
++        <string>${MACOSX_BUNDLE_GUI_IDENTIFIER}</string>
++        <key>CFBundleInfoDictionaryVersion</key>
++        <string>6.0</string>
++        <key>CFBundleLongVersionString</key>
++        <string>${MACOSX_BUNDLE_LONG_VERSION_STRING}</string>
++        <key>CFBundleName</key>
++        <string>${MACOSX_BUNDLE_BUNDLE_NAME}</string>
++        <key>CFBundlePackageType</key>
++        <string>APPL</string>
++        <key>CFBundleShortVersionString</key>
++        <string>${MACOSX_BUNDLE_SHORT_VERSION_STRING}</string>
++        <key>CFBundleSignature</key>
++        <string>????</string>
++        <key>CFBundleVersion</key>
++        <string>${MACOSX_BUNDLE_BUNDLE_VERSION}</string>
++        <key>CSResourcesFileMapped</key>
++        <true/>
++        <key>LSRequiresCarbon</key>
++        <true/>
++        <key>NSHumanReadableCopyright</key>
++        <string>${MACOSX_BUNDLE_COPYRIGHT}</string>
++        <key>LSMultipleInstancesProhibited</key>
++        <true/>
++        <key>CFBundleDocumentTypes</key>
++        <array>
++            <dict>
++                <key>CFBundleTypeExtensions</key>
++                <array>
++                        <string>*</string>
++                </array>
++                <key>CFBundleTypeName</key>
++                <string>NSStringPboardType</string>
++                <key>CFBundleTypeRole</key>
++                <string>Editor</string>
++            </dict>
++        </array>
++</dict>
++</plist>
