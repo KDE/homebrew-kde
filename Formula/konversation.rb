83ee65e91c52a7bf7a9283ec8d4f319ee4784c58
Darcy Shen <sadhen@zoho.com>
2018-11-21 00:36:13 +0800

    A Retina Konversation (#271)

--- Formula/konversation.rb
+++ Formula/konversation.rb
@@ -3,6 +3,7 @@ class Konversation < Formula
   homepage "https://www.kde.org"
   url "https://download.kde.org/stable/konversation/1.7.5/src/konversation-1.7.5.tar.xz"
   sha256 "60bf7533062b5fc63a37105461b4776437f4e24859e8ddaed1d48c4ba1470940"
+  revision 1
 
   head "git://anongit.kde.org/konversation.git"
 
@@ -17,6 +18,8 @@ class Konversation < Formula
   depends_on "KDE-mac/kde/kf5-knotifyconfig"
   depends_on "KDE-mac/kde/kf5-kparts"
   depends_on "qca"
+
+  patch :DATA
 
   def install
     args = std_cmake_args
@@ -51,3 +54,76 @@ class Konversation < Formula
   EOS
   end
 end
+
+__END__
+diff --git a/src/CMakeLists.txt b/src/CMakeLists.txt
+index 04f28971..5387a038 100644
+--- a/src/CMakeLists.txt
++++ b/src/CMakeLists.txt
+@@ -249,4 +249,5 @@ if (WIN32)
+   target_link_libraries(konversation ws2_32) # for symbols from winsock2.h: ntohl, etc.
+ endif()
+
++set_target_properties(konversation PROPERTIES MACOSX_BUNDLE_INFO_PLIST ${CMAKE_CURRENT_SOURCE_DIR}/MacOSXBundleInfo.plist.in)
+ install(TARGETS konversation ${INSTALL_TARGETS_DEFAULT_ARGS})
+diff --git a/src/MacOSXBundleInfo.plist.in b/src/MacOSXBundleInfo.plist.in
+new file mode 100644
+index 00000000..ab421eaf
+--- /dev/null
++++ b/src/MacOSXBundleInfo.plist.in
+@@ -0,0 +1,55 @@
++<?xml version="1.0" encoding="UTF-8"?>
++<!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
++<plist version="1.0">
++<dict>
++        <key>NSPrincipalClass</key>
++        <string>NSApplication</string>
++        <key>NSHighResolutionCapable</key>
++        <string>True</string>
++        <key>CFBundleDevelopmentRegion</key>
++        <string>English</string>
++        <key>CFBundleExecutable</key>
++        <string>${MACOSX_BUNDLE_EXECUTABLE_NAME}</string>
++        <key>CFBundleGetInfoString</key>
++        <string>${MACOSX_BUNDLE_INFO_STRING}</string>
++        <key>CFBundleIconFile</key>
++        <string>${MACOSX_BUNDLE_ICON_FILE}</string>
++        <key>CFBundleIdentifier</key>
++        <string>${MACOSX_BUNDLE_GUI_IDENTIFIER}</string>
++        <key>CFBundleInfoDictionaryVersion</key>
++        <string>6.0</string>
++        <key>CFBundleLongVersionString</key>
++        <string>${MACOSX_BUNDLE_LONG_VERSION_STRING}</string>
++        <key>CFBundleName</key>
++        <string>${MACOSX_BUNDLE_BUNDLE_NAME}</string>
++        <key>CFBundlePackageType</key>
++        <string>APPL</string>
++        <key>CFBundleShortVersionString</key>
++        <string>${MACOSX_BUNDLE_SHORT_VERSION_STRING}</string>
++        <key>CFBundleSignature</key>
++        <string>????</string>
++        <key>CFBundleVersion</key>
++        <string>${MACOSX_BUNDLE_BUNDLE_VERSION}</string>
++        <key>CSResourcesFileMapped</key>
++        <true/>
++        <key>LSRequiresCarbon</key>
++        <true/>
++        <key>NSHumanReadableCopyright</key>
++        <string>${MACOSX_BUNDLE_COPYRIGHT}</string>
++        <key>LSMultipleInstancesProhibited</key>
++        <true/>
++        <key>CFBundleDocumentTypes</key>
++        <array>
++            <dict>
++                <key>CFBundleTypeExtensions</key>
++                <array>
++                        <string>*</string>
++                </array>
++                <key>CFBundleTypeName</key>
++                <string>NSStringPboardType</string>
++                <key>CFBundleTypeRole</key>
++                <string>Editor</string>
++            </dict>
++        </array>
++</dict>
++</plist>
