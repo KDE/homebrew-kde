f1057d7a9199390d40ac922e54d302b9048ffa76
Gustavo Alvarez <sl1pkn07@gmail.com>
2019-03-17 14:29:47 +0100

    Libkexiv2: fix build with Exiv2 0.27
    Fix build with Exiv2 0.27
    
    https://cgit.kde.org/libkexiv2.git/diff/?id=d1be9298c7f2b9c717cda154d41732f230ea13a6
    
    (patch taked from github mirror)

--- Formula/libkexiv2.rb
+++ Formula/libkexiv2.rb
@@ -4,7 +4,13 @@ class Libkexiv2 < Formula
   url "https://download.kde.org/stable/applications/18.12.3/src/libkexiv2-18.12.3.tar.xz"
   sha256 "1d14ff63af42ab7e19e2039648a95ea5dc946afbe3e3df52c17ce1618a02ebdc"
 
-  revision 1
+  patch do
+    # Fix build with Exiv2 0.27
+    url "https://github.com/KDE/libkexiv2/commit/d1be9298c7f2b9c717cda154d41732f230ea13a6.patch"
+    sha256 "e47869b9ab5640d0ead20d645b0bdf07cec3f4a5b52749b5de303c6dd27faf8c"
+  end
+  
+  revision 2
   head "git://anongit.kde.org/libkexiv2.git"
   depends_on "cmake" => :build
   depends_on "KDE-mac/kde/kf5-extra-cmake-modules" => :build
