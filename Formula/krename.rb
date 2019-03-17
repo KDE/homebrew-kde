6c7530228d5884dbd31b20166c0126a4891eefd7
Gustavo Alvarez <sl1pkn07@gmail.com>
2019-03-17 20:07:18 +0100

    krename: some rework
    some rework

--- Formula/krename.rb
+++ Formula/krename.rb
@@ -7,12 +7,13 @@ class Krename < Formula
   revision 1
   head "git://anongit.kde.org/krename.git"
   depends_on "cmake" => :build
-  depends_on "kf5-extra-cmake-modules" => :build
+  depends_on "KDE-mac/kde/kf5-extra-cmake-modules" => :build
   depends_on "ninja" => :build
-  depends_on "exiv2"
+  #  depends_on "exiv2"
   depends_on "hicolor-icon-theme"
-  depends_on "kf5-kio"
-  depends_on "kf5-kjs"
+  depends_on "KDE-mac/kde/kf5-breeze-icons"
+  depends_on "KDE-mac/kde/kf5-kio"
+  depends_on "KDE-mac/kde/kf5-kjs"
   depends_on "podofo"
   depends_on "taglib"
 
@@ -26,16 +27,21 @@ class Krename < Formula
       system "ninja", "install"
       prefix.install "install_manifest.txt"
     end
+    # Extract Qt plugin path
+    qtpp = `#{Formula["qt"].bin}/qtpaths --plugin-dir`.chomp
+    system "/usr/libexec/PlistBuddy",
+    "-c", "Add :LSEnvironment:QT_PLUGIN_PATH string \"#{qtpp}\:#{HOMEBREW_PREFIX}/lib/qt5/plugins\"",
+    "#{bin}/krename.app/Contents/Info.plist"
   end
 
   def post_install
-    mkdir_p "#{HOMEBREW_PREFIX}/share/krename"
-    ln_sf "#{HOMEBREW_PREFIX}/share/icons/breeze/breeze-icons.rcc", "#{HOMEBREW_PREFIX}/share/krename/icontheme.rcc"
+    ln_sf HOMEBREW_PREFIX/"share/icons/breeze/breeze-icons.rcc", HOMEBREW_PREFIX/"share/krename/icontheme.rcc"
   end
 
   def caveats; <<~EOS
     You need to take some manual steps in order to make this formula work:
        ln -sfv "$(brew --prefix)/share/krename" "$HOME/Library/Application Support"
+       ln -sfv "$(brew --prefix)/share/kservices5" "$HOME/Library/Application Support"
        mkdir -pv "$HOME/Applications/KDE"
        ln -sfv "$(brew --prefix)/opt/krename/bin/krename.app" "$HOME/Applications/KDE"
   EOS
