8b706e2ae6e4d8b2e67e244468e3363af078dc1f
Gustavo Alvarez <sl1pkn07@gmail.com>
2019-03-23 22:14:14 +0100

    kf5-sonnet: remove patch seems fixed in upstream: https://bugreports.qt.io/browse/QTBUG-61034 https://codereview.qt-project.org/#/c/196062/ https://codereview.qt-project.org/#/c/196065/

--- Formula/kf5-sonnet.rb
+++ Formula/kf5-sonnet.rb
@@ -18,8 +18,6 @@ class Kf5Sonnet < Formula
 
   conflicts_with "hunspell", :because => "fatal error: 'hunspell.hxx' file not found"
 
-  patch :DATA
-
   def install
     args = std_cmake_args
     args << "-DBUILD_TESTING=OFF"
@@ -42,16 +40,3 @@ class Kf5Sonnet < Formula
   end
 end
 
-__END__
-diff --git a/src/plugins/nsspellchecker/CMakeLists.txt b/src/plugins/nsspellchecker/CMakeLists.txt
-index 5e80a10..b1e590d 100644
---- a/src/plugins/nsspellchecker/CMakeLists.txt
-+++ b/src/plugins/nsspellchecker/CMakeLists.txt
-@@ -7,5 +7,7 @@ add_library(sonnet_nsspellchecker MODULE ${sonnet_nsspellchecker_PART_SRCS})
- 
- target_link_libraries(sonnet_nsspellchecker PRIVATE KF5::SonnetCore "-framework Cocoa")
- 
-+target_compile_definitions(sonnet_nsspellchecker PRIVATE "QT_NO_EXCEPTIONS")
-+
- install(TARGETS sonnet_nsspellchecker  DESTINATION ${KDE_INSTALL_PLUGINDIR}/kf5/sonnet/)
- 
