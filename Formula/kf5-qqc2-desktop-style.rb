1955528926665ce7c182659f2404c482ed438787
Gustavo Alvarez <sl1pkn07@gmail.com>
2019-04-28 03:45:32 +0200

    Update kf5-qqc2-desktop-style.rb
    fix build

--- Formula/kf5-qqc2-desktop-style.rb
+++ Formula/kf5-qqc2-desktop-style.rb
@@ -8,15 +8,18 @@ class Kf5Qqc2DesktopStyle < Formula
 
   depends_on "cmake" => :build
   depends_on "KDE-mac/kde/kf5-extra-cmake-modules" => :build
-  depends_on "KDE-mac/kde/kf5-kconfigwidgets" => :build
+  depends_on "KDE-mac/kde/kf5-kcoreaddons" => :build
   depends_on "KDE-mac/kde/kf5-kiconthemes" => :build
   depends_on "KDE-mac/kde/kf5-kirigami2" => :build
   depends_on "ninja" => :build
+
   depends_on "qt"
 
   def install
     args = std_cmake_args
     args << "-DBUILD_TESTING=OFF"
+    args << "-DKDE_INSTALL_QMLDIR=lib/qt5/qml"
+    args << "-DKDE_INSTALL_PLUGINDIR=lib/qt5/plugins"
 
     mkdir "build" do
       system "cmake", "-G", "Ninja", "..", *args
