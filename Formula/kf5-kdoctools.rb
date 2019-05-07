270d33281b4187d562ec8764150983f5d73ac34a
Gustavo Alvarez <sl1pkn07@gmail.com>
2019-05-07 20:22:10 +0200

    Update kf5-kdoctools.rb
    add build documentation

--- Formula/kf5-kdoctools.rb
+++ Formula/kf5-kdoctools.rb
@@ -3,10 +3,11 @@ class Kf5Kdoctools < Formula
   homepage "https://www.kde.org"
   url "https://download.kde.org/stable/frameworks/5.57/kdoctools-5.57.0.tar.xz"
   sha256 "649dbaff4f1559302e7da07f423a0bc9e3faa1c7a93dfeb170e50bf452d8def2"
-
+  revision 1
   head "git://anongit.kde.org/kdoctools.git"
 
   depends_on "cmake" => :build
+  depends_on "doxygen" => :build
   depends_on "gettext" => :build
   depends_on "KDE-mac/kde/kf5-extra-cmake-modules" => :build
   depends_on "KDE-mac/kde/kf5-ki18n" => :build
@@ -21,6 +22,7 @@ class Kf5Kdoctools < Formula
   def install
     args = std_cmake_args
     args << "-DBUILD_TESTING=OFF"
+    args << "-DBUILD_QCH=ON"
     args << "-DKDE_INSTALL_QMLDIR=lib/qt5/qml"
     args << "-DKDE_INSTALL_PLUGINDIR=lib/qt5/plugins"
     args << "-DKDE_INSTALL_QTPLUGINDIR=lib/qt5/plugins"
