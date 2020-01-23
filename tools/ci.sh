b443665e26121bf201f01e981c3effac9ce08457
Yurii Kolesnykov <root@yurikoles.com>
2020-01-23 18:52:55 +0200

    fix upgrades in ci
    Signed-off-by: Yurii Kolesnykov <root@yurikoles.com>

--- tools/ci.sh
+++ tools/ci.sh
@@ -4,7 +4,11 @@ for file in "$@"; do
     if [[ $file =~ .rb$ ]] ; then
 		brew style $file
 		brew audit --strict $file
-		brew install $file
+		if brew list|grep ${file#.rb} ; then
+			brew upgrade -vd $file
+		else
+			brew install $file
+		fi
 		brew test $file
 		brew audit --strict $file
     fi
