d5f59bd1f042015160e79ef3e1bba4bbb4fe739e
Yurii Kolesnykov <root@yurikoles.com>
2020-04-23 12:09:16 +0300

    fix circular dependencies in uninstall script
    Signed-off-by: Yurii Kolesnykov <root@yurikoles.com>

--- tools/uninstall.sh
+++ tools/uninstall.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-brew uninstall -f `brew list --full-name -1 | grep '^kde-mac/kde'`
+brew uninstall -f --ignore-dependencies `brew list --full-name -1 | grep '^kde-mac/kde'`
 rm -rf ~/Applications/KDE
