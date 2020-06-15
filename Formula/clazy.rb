e80e2189152fec184f8503fd5401ef90f4c50f6d
Yurii Kolesnykov <root@yurikoles.com>
2020-06-15 21:10:21 +0300

    clazy: force head, since 1.6 doesn't build with recent llvm
    Signed-off-by: Yurii Kolesnykov <root@yurikoles.com>

--- Formula/clazy.rb
+++ Formula/clazy.rb
@@ -1,8 +1,6 @@
 class Clazy < Formula
   desc "Qt oriented code checker based on clang framework"
   homepage "https://github.com/KDE/clazy"
-  url "https://download.kde.org/stable/clazy/1.6/src/clazy-1.6.tar.xz"
-  sha256 "806d1befaddba82316f4ea34f1cba38bcc7545d0981007e3527c1ff1576dd0de"
   head "https://invent.kde.org/sdk/clazy.git"
 
   depends_on "cmake" => [:build, :test]
@@ -12,12 +10,8 @@ class Clazy < Formula
   depends_on "qt"
 
   def install
-    args = std_cmake_args
-    args << "-DBUILD_TESTING=OFF"
-    args << "-DCMAKE_INSTALL_MANDIR=#{man}"
-
     mkdir "build" do
-      system "cmake", "-G", "Ninja", "..", *args
+      system "cmake", "-G", "Ninja", "..", *std_cmake_args
       system "ninja"
       system "ninja", "install"
       prefix.install "install_manifest.txt"
@@ -25,6 +19,6 @@ class Clazy < Formula
   end
 
   test do
-    assert `"#{bin}/clazy-standalone" --help | grep -- -help` =~ /-help/
+    assert_match "help", shell_output("#{bin}/clazy-standalone --help")
   end
 end
