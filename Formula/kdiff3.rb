7263a6a71e4051a3502e593e894f8840e33415f0
Yurii Kolesnykov <root@yurikoles.com>
2020-06-29 23:41:41 +0300

    kdiff 1.8.3
    Signed-off-by: Yurii Kolesnykov <root@yurikoles.com>

--- Formula/kdiff3.rb
+++ Formula/kdiff3.rb
@@ -1,8 +1,8 @@
 class Kdiff3 < Formula
   desc "File And Folder Comparison And Merge Tool"
   homepage "https://kde.org/applications/en/development/org.kde.kdiff3"
-  url "https://download.kde.org/stable/kdiff3/kdiff3-1.8.2.tar.xz"
-  sha256 "fb7154778491dc11b9bd38465164ce1a7c2d1810f70a57cd4268ffad42d5bb68"
+  url "https://download.kde.org/stable/kdiff3/kdiff3-1.8.3.tar.xz"
+  sha256 "2f35e9357a303144e3dd5a033a1a8caa70d3eecdb355213e9473cd04bc308bab"
   head "https://invent.kde.org/sdk/kdiff3.git"
 
   depends_on "cmake" => :build
