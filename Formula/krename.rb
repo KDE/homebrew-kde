5b2d0c9523d7eb842d28c260d88625bd5301a1cf
Yurii Kolesnykov <root@yurikoles.com>
2020-07-12 21:41:19 +0300

    krename conflicts with taglib
    Signed-off-by: Yurii Kolesnykov <root@yurikoles.com>

--- Formula/krename.rb
+++ Formula/krename.rb
@@ -3,11 +3,13 @@ class Krename < Formula
   homepage "https://userbase.kde.org/KRename"
   url "https://download.kde.org/stable/krename/5.0.0/src/krename-5.0.0.tar.xz"
   sha256 "0a61761853787fd46b35f3a733cf87cde00de5df631728332a64c38c670bd28c"
+  revision 1
   head "https://invent.kde.org/utilities/krename.git"
 
   depends_on "cmake" => [:build, :test]
   depends_on "kde-extra-cmake-modules" => [:build, :test]
   depends_on "ninja" => :build
+  depends_on "pkg-config" => :build
 
   depends_on "exiv2"
   depends_on "hicolor-icon-theme"
@@ -15,7 +17,8 @@ class Krename < Formula
   depends_on "KDE-mac/kde/kf5-kio"
   depends_on "KDE-mac/kde/kf5-kjs"
   depends_on "podofo"
-  depends_on "taglib"
+
+  conflicts_with "taglib", :because => "linking errors"
 
   def install
     args = std_cmake_args
