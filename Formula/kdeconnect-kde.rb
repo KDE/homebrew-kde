af66d662bca37615e22189f37acdc3db64d29619
Yurii Kolesnykov <root@yurikoles.com>
2021-01-13 17:44:08 +0200

    rename kdeconnect to kdeconnect-kde
    Signed-off-by: Yurii Kolesnykov <root@yurikoles.com>

--- Formula/kdeconnect-kde.rb
+++ Formula/kdeconnect-kde.rb
@@ -1,9 +1,8 @@
-class Kdeconnect < Formula
-  desc "Adds communication between KDE and your smartphone"
+class KdeconnectKde < Formula
+  desc "Multi-platform app that allows your devices to communicate"
   homepage "https://community.kde.org/KDEConnect"
-  url "https://download.kde.org/stable/kdeconnect/1.4/kdeconnect-kde-1.4.tar.xz"
+  url "https://download.kde.org/stable/release-service/20.12.0/src/kdeconnect-kde-20.12.0.tar.xz"
   sha256 "caee7945a9d9bb881a943dc8d2fd0d702c04da5bdb2df14d4f875e7cf5d5261a"
-  revision 1
   head "https://github.com/KDE/kdeconnect-kde.git"
 
   depends_on "cmake" => [:build, :test]
@@ -16,15 +15,15 @@ class Kdeconnect < Formula
   depends_on "KDE-mac/kde/kf5-kcmutils"
   depends_on "KDE-mac/kde/kf5-kconfigwidgets"
   depends_on "KDE-mac/kde/kf5-kdbusaddons"
+  depends_on "KDE-mac/kde/kf5-kdoctools"
   depends_on "KDE-mac/kde/kf5-kiconthemes"
   depends_on "KDE-mac/kde/kf5-kio"
   depends_on "KDE-mac/kde/kf5-kirigami2"
   depends_on "KDE-mac/kde/kf5-knotifications"
-  depends_on "KDE-mac/kde/kf5-kservice"
   depends_on "KDE-mac/kde/kf5-kpeople"
+  depends_on "KDE-mac/kde/kf5-kservice"
   depends_on "qca"
   depends_on "qt"
-  depends_on "KDE-mac/kde/kf5-kdoctools" => :optional
 
   def install
     args = std_cmake_args
