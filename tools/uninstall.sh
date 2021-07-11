ebba46df50c3f0536602365f0394e37ebaa541b1
Yurii Kolesnykov <root@yurikoles.com>
2021-07-12 02:41:37 +0300

    impove uninstall script
    Signed-off-by: Yurii Kolesnykov <root@yurikoles.com>

--- tools/uninstall.sh
+++ tools/uninstall.sh
@@ -1,7 +1,34 @@
 #!/bin/bash
 
-brew update
-export HOMEBREW_NO_AUTO_UPDATE=1
+HOMEBREW_NO_AUTO_UPDATE=1
+INSTALLED_FORMULAS_LIST=/tmp/kde/installed
+mkdir -p /tmp/kde/
 
-brew uninstall -f --ignore-dependencies `brew ls --formula --full-name | grep '^kde-mac/kde'`
-rm -rf ~/Applications/KDE
+brew ls --formula --full-name > "${INSTALLED_FORMULAS_LIST}"
+
+if grep -q '^kde-mac/kde' "${INSTALLED_FORMULAS_LIST}"; then
+	echo "Uninstalling formulas from kde-mac/kde tap"
+	brew uninstall -f `grep '^kde-mac/kde' "${INSTALLED_FORMULAS_LIST}"`
+else
+	echo "No formulas from kde-mac/kde tap is installed"
+fi
+
+CORE_FORMULAS=(
+	threadweaver
+	kdoctools
+	ki18n
+	karchive
+	extra-cmake-modules
+)
+
+echo "Removing kde formulas from homebrew/core if any installed"
+
+for CORE_FORMULA in "${CORE_FORMULAS[@]}"; do
+	if grep -q "${CORE_FORMULA}" "${INSTALLED_FORMULAS_LIST}"; then
+		brew uninstall -f --formula "${CORE_FORMULA}"
+	fi
+done
+
+rm "${INSTALLED_FORMULAS_LIST}"
+echo "Cleaning ~/Applications/KDE"
+rm -rf "${HOME}"/Applications/KDE
