5ba1f5671b0755d9d1cd9b21c17bc037b0819f20
Yurii Kolesnykov <root@yurikoles.com>
2021-07-19 18:23:12 +0300

    kf5-kimageformats fix openexr
    Signed-off-by: Yurii Kolesnykov <root@yurikoles.com>

--- Formula/kf5-kimageformats.rb
+++ Formula/kf5-kimageformats.rb
@@ -3,9 +3,18 @@ require_relative "../lib/cmake"
 class Kf5Kimageformats < Formula
   desc "Image format plugins for Qt5"
   homepage "https://api.kde.org/frameworks/kimageformats/html/index.html"
-  url "https://download.kde.org/stable/frameworks/5.84/kimageformats-5.84.0.tar.xz"
-  sha256 "badd15c93c1af2c6ab7f156245186a2223a184523070c6614328486e186eedf7"
+  revision 1
   head "https://invent.kde.org/frameworks/kimageformats.git"
+
+  stable do
+    url "https://download.kde.org/stable/frameworks/5.84/kimageformats-5.84.0.tar.xz"
+    sha256 "badd15c93c1af2c6ab7f156245186a2223a184523070c6614328486e186eedf7"
+
+    patch do # should be removed in 5.85.0
+      url "https://invent.kde.org/frameworks/kimageformats/-/commit/5aa03c12add871996aaa865adf32554e4174ed06.diff"
+      sha256 "2ceb47d4f5aaa9c0be75b061fc39e101f819c611a72b4e9b2f83732b5a342331"
+    end
+  end
 
   livecheck do
     url :head
@@ -18,9 +27,8 @@ class Kf5Kimageformats < Formula
 
   depends_on "jasper"
   depends_on "karchive"
+  depends_on "openexr"
   depends_on "qt@5"
-
-  conflicts_with "openexr", because: "compile errors"
 
   def install
     args = kde_cmake_args
