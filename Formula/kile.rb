f6b5c388806c3128d3fcc0934103e3e16a9c1783
Yurii Kolesnykov <root@yurikoles.com>
2021-08-22 13:04:45 +0300

    kile 2.9.93_2: fix cmake script, proper deps
    Signed-off-by: Yurii Kolesnykov <root@yurikoles.com>

--- Formula/kile.rb
+++ Formula/kile.rb
@@ -5,7 +5,7 @@ class Kile < Formula
   homepage "https://kile.sourceforge.io"
   url "https://downloads.sourceforge.net/project/kile/unstable/kile-3.0b3/kile-2.9.93.tar.bz2"
   sha256 "04499212ffcb24fb3a6829149a7cae4c6ad5d795985f080800d6df72f88c5df0"
-  revision 1
+  revision 2
   head "https://invent.kde.org/office/kile.git"
 
   livecheck do
@@ -18,17 +18,20 @@ class Kile < Formula
   depends_on "kdoctools" => :build
   depends_on "ninja" => :build
 
+  depends_on "kde-mac/kde/kf5-khtml"
   depends_on "kde-mac/kde/kf5-kinit"
   depends_on "kde-mac/kde/kf5-ktexteditor"
-  depends_on "kde-mac/kde/konsole"
   depends_on "kde-mac/kde/okular"
+
+  depends_on "kde-mac/kde/konsole" => :recommended
+  depends_on "poppler-qt5" => :recommended
 
   def install
     args = kde_cmake_args
 
     system "cmake", *args
-    system "cmake", "--build", "."
-    system "cmake", "--install", "."
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
     prefix.install "build/install_manifest.txt"
 
     # Extract Qt plugin path
