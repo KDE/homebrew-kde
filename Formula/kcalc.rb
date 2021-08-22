d28e96889c418034db6f61f181bf0db1e79fe44c
Yurii Kolesnykov <root@yurikoles.com>
2021-08-22 14:14:35 +0300

    kcalc: rebase and send patch
    Signed-off-by: Yurii Kolesnykov <root@yurikoles.com>

--- Formula/kcalc.rb
+++ Formula/kcalc.rb
@@ -21,7 +21,10 @@ class Kcalc < Formula
   depends_on "kde-mac/kde/kf5-kinit"
   depends_on "mpfr"
 
-  patch :DATA
+  patch do
+    url "https://invent.kde.org/yurikoles/kcalc/-/commit/575dccae306c96456980e3ea0e28a503dc35299f.diff"
+    sha256 "3ea3897414e483525828f74282c76e1208664090ceee0e2dcd472241fe05c947"
+  end
 
   def install
     args = kde_cmake_args
@@ -53,80 +56,3 @@ class Kcalc < Formula
     assert_match "help", shell_output("#{bin}/kcalc.app/Contents/MacOS/kcalc --help")
   end
 end
-
-# Fix retina display
-
-__END__
-diff --git a/CMakeLists.txt b/CMakeLists.txt
-index 4d42e2e..b7fac59 100644
---- a/CMakeLists.txt
-+++ b/CMakeLists.txt
-@@ -123,6 +123,7 @@ target_link_libraries(kdeinit_kcalc
- install(TARGETS kdeinit_kcalc ${KDE_INSTALL_TARGETS_DEFAULT_ARGS})
-
- target_link_libraries( kcalc kdeinit_kcalc )
-+set_target_properties( kcalc PROPERTIES MACOSX_BUNDLE_INFO_PLIST ${CMAKE_CURRENT_SOURCE_DIR}/MacOSXBundleInfo.plist.in )
- install(TARGETS kcalc  ${KDE_INSTALL_TARGETS_DEFAULT_ARGS} )
-
- ########### install files ###############
-diff --git a/MacOSXBundleInfo.plist.in b/MacOSXBundleInfo.plist.in
-new file mode 100644
-index 0000000..ab421ea
---- /dev/null
-+++ b/MacOSXBundleInfo.plist.in
-@@ -0,0 +1,55 @@
-+<?xml version="1.0" encoding="UTF-8"?>
-+<!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-+<plist version="1.0">
-+<dict>
-+        <key>NSPrincipalClass</key>
-+        <string>NSApplication</string>
-+        <key>NSHighResolutionCapable</key>
-+        <string>True</string>
-+        <key>CFBundleDevelopmentRegion</key>
-+        <string>English</string>
-+        <key>CFBundleExecutable</key>
-+        <string>${MACOSX_BUNDLE_EXECUTABLE_NAME}</string>
-+        <key>CFBundleGetInfoString</key>
-+        <string>${MACOSX_BUNDLE_INFO_STRING}</string>
-+        <key>CFBundleIconFile</key>
-+        <string>${MACOSX_BUNDLE_ICON_FILE}</string>
-+        <key>CFBundleIdentifier</key>
-+        <string>${MACOSX_BUNDLE_GUI_IDENTIFIER}</string>
-+        <key>CFBundleInfoDictionaryVersion</key>
-+        <string>6.0</string>
-+        <key>CFBundleLongVersionString</key>
-+        <string>${MACOSX_BUNDLE_LONG_VERSION_STRING}</string>
-+        <key>CFBundleName</key>
-+        <string>${MACOSX_BUNDLE_BUNDLE_NAME}</string>
-+        <key>CFBundlePackageType</key>
-+        <string>APPL</string>
-+        <key>CFBundleShortVersionString</key>
-+        <string>${MACOSX_BUNDLE_SHORT_VERSION_STRING}</string>
-+        <key>CFBundleSignature</key>
-+        <string>????</string>
-+        <key>CFBundleVersion</key>
-+        <string>${MACOSX_BUNDLE_BUNDLE_VERSION}</string>
-+        <key>CSResourcesFileMapped</key>
-+        <true/>
-+        <key>LSRequiresCarbon</key>
-+        <true/>
-+        <key>NSHumanReadableCopyright</key>
-+        <string>${MACOSX_BUNDLE_COPYRIGHT}</string>
-+        <key>LSMultipleInstancesProhibited</key>
-+        <true/>
-+        <key>CFBundleDocumentTypes</key>
-+        <array>
-+            <dict>
-+                <key>CFBundleTypeExtensions</key>
-+                <array>
-+                        <string>*</string>
-+                </array>
-+                <key>CFBundleTypeName</key>
-+                <string>NSStringPboardType</string>
-+                <key>CFBundleTypeRole</key>
-+                <string>Editor</string>
-+            </dict>
-+        </array>
-+</dict>
-+</plist>
