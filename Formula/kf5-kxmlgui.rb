9ed65a6a845091af6d7e07441b05be81b3ebf5f1
Yurii Kolesnykov <root@yurikoles.com>
2022-01-03 01:32:32 +0200

    kf5-kxmlgui: disable BUILD_QCH
    BUG: 446492
    
    Closes #37
    
    Signed-off-by: Yurii Kolesnykov <root@yurikoles.com>

--- Formula/kf5-kxmlgui.rb
+++ Formula/kf5-kxmlgui.rb
@@ -5,10 +5,12 @@ class Kf5Kxmlgui < Formula
   homepage "https://api.kde.org/frameworks/kxmlgui/html/index.html"
   url "https://download.kde.org/stable/frameworks/5.89/kxmlgui-5.89.0.tar.xz"
   sha256 "a6c8938fa2f75c4b9b8c5badd1192a373e2c0b971db17ae5f85ce7d420784e56"
+  revision 1
   head "https://invent.kde.org/frameworks/kxmlgui.git", branch: "master"
 
   depends_on "cmake" => [:build, :test]
-  depends_on "doxygen" => :build
+  # https://bugs.kde.org/show_bug.cgi?id=446492
+  # depends_on "doxygen" => :build
   depends_on "extra-cmake-modules" => [:build, :test]
   depends_on "graphviz" => :build
   depends_on "ninja" => :build
@@ -20,6 +22,7 @@ class Kf5Kxmlgui < Formula
 
   def install
     args = kde_cmake_args
+    args << "-D BUILD_QCH=OFF" # https://bugs.kde.org/show_bug.cgi?id=446492
 
     system "cmake", *args
     system "cmake", "--build", "build"
@@ -28,7 +31,23 @@ class Kf5Kxmlgui < Formula
   end
 
   test do
+    args = std_cmake_args
+    args << "-Wno-dev"
+
+    qt_modules = %w[
+      DBus
+      Network
+      PrintSupport
+      Svg
+      Widgets
+      Xml
+    ]
+
+    qt_modules.each do |qt_module|
+      args << "-DQt5#{qt_module}_DIR=#{Formula["qt@5"].opt_prefix}/lib/cmake/Qt5#{qt_module}"
+    end
+
     (testpath/"CMakeLists.txt").write("find_package(KF5XmlGui REQUIRED)")
-    system "cmake", ".", "-Wno-dev"
+    system "cmake", ".", *args
   end
 end
