5aa4c8959b44bf6e6cc0320047e8c2b324638414
Yurii Kolesnykov <root@yurikoles.com>
2022-05-17 18:19:21 +0300

    kf5-plasma-framework: remove merged patch

--- Formula/kf5-plasma-framework.rb
+++ Formula/kf5-plasma-framework.rb
@@ -45,31 +45,3 @@ class Kf5PlasmaFramework < Formula
     system "cmake", ".", "-Wno-dev"
   end
 end
-
-# Mark executables as nongui type
-
-__END__
-diff --git a/CMakeLists.txt b/CMakeLists.txt
-index fb7b76b..71ee8a4 100644
---- a/CMakeLists.txt
-+++ b/CMakeLists.txt
-@@ -25,6 +25,7 @@ include(ECMAddQch)
- include(KDEPackageAppTemplates)
- include(ECMGenerateQmlTypes)
- include(ECMSetupQtPluginMacroNames)
-+include(ECMMarkNonGuiExecutable)
-
- option(BUILD_QCH "Build API documentation in QCH format (for e.g. Qt Assistant, Qt Creator & KDevelop)" OFF)
- add_feature_info(QCH ${BUILD_QCH} "API documentation in QCH format (for e.g. Qt Assistant, Qt Creator & KDevelop)")
-diff --git a/src/plasmapkg/CMakeLists.txt b/src/plasmapkg/CMakeLists.txt
-index 6247f92..20186d7 100644
---- a/src/plasmapkg/CMakeLists.txt
-+++ b/src/plasmapkg/CMakeLists.txt
-@@ -2,5 +2,6 @@ add_executable(plasmapkg2 main.cpp)
-
- target_link_libraries(plasmapkg2 Qt5::Core)
-
-+ecm_mark_nongui_executable(plasmapkg2)
- install(TARGETS plasmapkg2 ${KF5_INSTALL_TARGETS_DEFAULT_ARGS})
-
-
