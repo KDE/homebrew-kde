126e276f91dd81e696abd0def925f8ca8399057b
Max Leiter <maxwell.leiter@gmail.com>
2022-06-12 16:58:06 -0700

    kf5-kuserfeedback 1.2.0

---
+++ Formula/kf5-kuserfeedback.rb
@@ -0,0 +1,34 @@
+require_relative "../lib/cmake"
+
+class Kf5Kuserfeedback < Formula
+  desc "Framework for collecting user feedback for applications via telemetry and surveys"
+  homepage "https://api.kde.org/frameworks/kuserfeedback/html/index.html"
+  url "https://download.kde.org/stable/kuserfeedback/kuserfeedback-1.2.0.tar.xz"
+  sha256 "76aac922b153249b274680a6f4c72c238ef14e3df04bad00cb64158b1063f264"
+  head "https://invent.kde.org/libraries/kuserfeedback.git", branch: "master"
+
+  depends_on "cmake" => [:build, :test]
+  depends_on "doxygen" => :build
+  depends_on "extra-cmake-modules" => [:build, :test]
+  depends_on "ninja" => :build
+  depends_on "bison" => :build
+
+  depends_on "qt@5"
+
+  def install
+    args = kde_cmake_args
+
+    # Necessary so we use homebrew bison and not the system one
+    args << ("-DCMAKE_PREFIX_PATH=" + Formula["bison"].opt_prefix)
+
+    system "cmake", *args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+    prefix.install "build/install_manifest.txt"
+  end
+
+  test do
+    (testpath/"CMakeLists.txt").write("find_package(UserFeedback REQUIRED)")
+    system "cmake", ".", "-Wno-dev"
+  end
+end
