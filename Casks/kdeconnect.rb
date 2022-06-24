9715a500848dd02200a171b9b1a1ea43b5dd0456
Yurii Kolesnykov <root@yurikoles.com>
2022-06-24 17:23:54 +0300

    kdeconnect: lint
    Signed-off-by: Yurii Kolesnykov <root@yurikoles.com>

--- Casks/kdeconnect.rb
+++ Casks/kdeconnect.rb
@@ -3,14 +3,14 @@ cask "kdeconnect" do
   sha256 :no_check
 
   url do
-    require 'open-uri'
+    require "open-uri"
     base_url = "https://binary-factory.kde.org/view/MacOS/job/kdeconnect-kde_Release_macos/lastStableBuild"
     version = URI(base_url.to_s)
-               .open
-               .read
-               .scan(/href=.*?kdeconnect-kde[._-]v?(\d+(?:[.-]\d+)+)-macos-64-clang\.dmg/i)
-               .flatten
-               .first # should only be one mach
+              .open
+              .read
+              .scan(/href=.*?kdeconnect-kde[._-]v?(\d+(?:[.-]\d+)+)-macos-64-clang\.dmg/i)
+              .flatten
+              .first # should only be one mach
     file = "kdeconnect-kde-#{version}-macos-64-clang.dmg"
     "#{base_url}/artifact/#{file}"
   end
@@ -18,13 +18,7 @@ cask "kdeconnect" do
   desc "Enabling communication between all your devices"
   homepage "https://kdeconnect.kde.org/"
 
-  livecheck do
-    url "https://binary-factory.kde.org/job/kdeconnect-kde_Release_macos/lastStableBuild/artifact/"
-    regex(/href=.*?kdeconnect-kde[._-]v?(\d+(?:[.-]\d+)+)-macos-64-clang\.dmg/i)
-  end
-
   app "kdeconnect-indicator.app", target: "KDE Connect.app"
-
   binary "#{appdir}/KDE Connect.app/Contents/MacOS/kdeconnect-cli",
          target: "kdeconnect"
 
