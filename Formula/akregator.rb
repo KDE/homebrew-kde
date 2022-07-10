179b612e846de1822923f7924935344c53ba1392
Max Leiter <maxwell.leiter@gmail.com>
2022-07-10 15:01:36 -0700

    Add libkelo and akregator formula

---
+++ Formula/akregator.rb
@@ -0,0 +1,31 @@
+class Akregator < Formula
+  desc "A KDE Feed Reader."
+  homepage "https://kontact.kde.org/components/akregator/"
+  url "https://download.kde.org/stable/release-service/22.04.3/src/akregator-22.04.3.tar.xz"
+  sha256 "2f5551c5ebfce6e43ee22d46891d2f44eecb76af38f3979775f1c470c408c722"
+  head "https://invent.kde.org/pim/akregator.git", branch: "master"
+
+  depends_on "cmake" => :build
+  depends_on "extra-cmake-modules" => :build
+  depends_on "ninja" => :build
+
+  depends_on "qt@5"
+
+  depends_on "kdoctools"
+  depends_on "kde-mac/kde/kf5-kuserfeedback"
+  depends_on "kde-mac/kde/kontact-interface"
+  depends_on "kde-mac/kde/kf5-kcmutils"
+  depends_on "kde-mac/kde/kf5-knotifyconfig"
+  depends_on "kde-mac/kde/grantlee"
+  depends_on "kde-mac/kde/kf5-libkleo"
+
+  def install
+    system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+  end
+
+  test do
+    assert_match "help", shell_output("#{bin}/akregator.app/Contents/MacOS/akregator --help")
+  end
+end
---
+++ Formula/kf5-libkleo.rb
@@ -0,0 +1,35 @@
+require_relative "../lib/cmake"
+
+class Kf5Libkleo < Formula
+  desc "KDE PIM cryptographic library"
+  homepage "https://api.kde.org/kdepim/libkleo/html/index.html"
+  url "https://download.kde.org/stable/release-service/22.04.3/src/libkleo-22.04.3.tar.xz"
+  sha256 "d76e6fd8740d17eea371c4052b17e98a7fdf77651d801a31701968f2455c665a"
+  head "https://invent.kde.org/pim/libkleo.git", branch: "master"
+
+  depends_on "cmake" => [:build, :test]
+  depends_on "extra-cmake-modules" => :build
+  depends_on "ninja" => :build
+  depends_on "boost" => :build
+  depends_on "doxygen" => :build
+
+  depends_on "qt@5"
+
+  depends_on "kde-mac/kde/kf5-kitemmodels"
+  depends_on "kde-mac/kde/kpimtextedit"
+  depends_on "gpgme"
+
+  def install
+    args = kde_cmake_args
+
+    system "cmake", *args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+    prefix.install "build/install_manifest.txt"
+  end
+
+  test do
+    (testpath/"CMakeLists.txt").write("find_package(KF5Libkleo REQUIRED)")
+    system "cmake", ".", "-Wno-dev"
+  end
+end
