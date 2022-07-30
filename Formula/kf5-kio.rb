bf08d171f9a3156b5045a97f7d56795a09ec2412
Yurii Kolesnykov <root@yurikoles.com>
2022-07-31 01:20:11 +0300

    kf5-kio fix deps
    Signed-off-by: Yurii Kolesnykov <root@yurikoles.com>

--- Formula/kf5-kio.rb
+++ Formula/kf5-kio.rb
@@ -8,8 +8,9 @@ class Kf5Kio < Formula
   head "https://invent.kde.org/frameworks/kio.git", branch: "master"
 
   depends_on "cmake" => [:build, :test]
-  depends_on "doxygen" => :build
   depends_on "extra-cmake-modules" => [:build, :test]
+
+  depends_on "doxygen" => :build
   depends_on "graphviz" => :build
   depends_on "kdoctools" => :build
   depends_on "ninja" => :build
