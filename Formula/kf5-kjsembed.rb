c4f9d77e8f1cd453b91885bbe0a8e23222ea3fe2
Yurii Kolesnykov <root@yurikoles.com>
2022-08-17 23:05:51 +0300

    kf5-kjsembed: remove merged patch
    Signed-off-by: Yurii Kolesnykov <root@yurikoles.com>

--- Formula/kf5-kjsembed.rb
+++ Formula/kf5-kjsembed.rb
@@ -16,8 +16,6 @@ class Kf5Kjsembed < Formula
   depends_on "kde-mac/kde/kf5-kjs"
   depends_on "ki18n"
 
-  patch :DATA
-
   def install
     system "cmake", *kde_cmake_args
     system "cmake", "--build", "build"
@@ -30,29 +28,3 @@ class Kf5Kjsembed < Formula
     system "cmake", ".", "-Wno-dev"
   end
 end
-
-# Mark executable as nongui type
-# https://invent.kde.org/frameworks/kjsembed/-/merge_requests/4
-__END__
-diff --git a/CMakeLists.txt b/CMakeLists.txt
-index 85973b9..a45ad84 100644
---- a/CMakeLists.txt
-+++ b/CMakeLists.txt
-@@ -20,6 +20,7 @@ include(ECMGenerateHeaders)
- include(ECMDeprecationSettings)
- include(GenerateExportHeader)
- include(CMakePackageConfigHelpers)
-+include(ECMMarkNonGuiExecutable)
- 
- set(REQUIRED_QT_VERSION 5.15.2)
- find_package(Qt5 "${REQUIRED_QT_VERSION}" CONFIG REQUIRED UiTools Widgets Xml Svg)
-diff --git a/src/kjscmd/CMakeLists.txt b/src/kjscmd/CMakeLists.txt
-index 3272269..4d599e8 100644
---- a/src/kjscmd/CMakeLists.txt
-+++ b/src/kjscmd/CMakeLists.txt
-@@ -13,4 +13,5 @@ target_link_libraries(kjscmd5
-     KF5::JsEmbed
- )
- 
-+ecm_mark_nongui_executable(kjscmd5)
- install(TARGETS kjscmd5 ${KF5_INSTALL_TARGETS_DEFAULT_ARGS})
