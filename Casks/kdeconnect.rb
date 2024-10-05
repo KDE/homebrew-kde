53cff344ce4697389a70a302a9fa4d9402f32557
Ian Chamberlain <ian.h.chamberlain@gmail.com>
2024-10-05 17:16:30 -0400

    Update recent kdeconnect and add livecheck
    URL stanzas are deprecated in favor of livecheck:
    https://github.com/Homebrew/homebrew-cask/pull/186501
    
    This pins the current latest version of `master` and adds a livecheck
    stanza to make updating easier. In the future if this were upstreamed to
    `homebrew-cask`, I believe the BrewTestBot could automatically update
    the version + sha256 using this livecheck.
    
    Reviewed-by: Yurii Kolesnykov <root@yurikoles.com>

---
+++ Casks/kdeconnect.rb
@@ -0,0 +1,36 @@
+cask "kdeconnect" do
+  arch arm: "arm64", intel: "x86_64"
+
+  # TODO: eventually also handle release versions (e.g. release-24.08 etc.)?
+  base_url = "https://cdn.kde.org/ci-builds/network/kdeconnect-kde/master/macos-#{arch}"
+
+  version "4458"
+  sha256 arm:   "f27cc172ca35498e890a302f7cbb18ffd3eded28810b849dcd047fc965499491",
+         intel: "2ab29fc26b0030577cba3bb83a952df2deaec04deb24bad6e3e66b27486255e0"
+
+  url "#{base_url}/kdeconnect-kde-master-#{version}-macos-clang-#{arch}.dmg"
+  name "KDE Connect"
+  desc "Enabling communication between all your devices"
+  homepage "https://kdeconnect.kde.org/"
+
+  livecheck do
+    url base_url
+    regex(/href=.*?kdeconnect-kde-master-(\d+(?:)+)-macos-clang-#{arch}\.dmg/i)
+  end
+
+  app "kdeconnect-indicator.app", target: "KDE Connect.app"
+  binary "#{appdir}/KDE Connect.app/Contents/MacOS/kdeconnect-cli",
+         target: "kdeconnect"
+
+  uninstall quit: "org.kde.kdeconnect"
+
+  zap trash: [
+    "~/Library/Application Support/kpeoplevcard",
+    "~/Library/Caches/kdeconnect.sms",
+    "~/Library/Preferences/kdeconnect",
+    "~/Library/Preferences/kdeconnect_runcommand",
+    "~/Library/Preferences/kdeconnect_sendnotifications",
+    "~/Library/Preferences/kdeconnect_share",
+    "~/Library/Preferences/org.kde.kdeconnect.plist",
+  ]
+end
