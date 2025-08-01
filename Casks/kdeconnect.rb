d4d10832bf4b2894eb82a24f85a4170535ea05d7
Yurii Kolesnykov <root@yurikoles.com>
2025-08-01 02:47:07 +0100

    kdeconnect 5234
    Signed-off-by: Yurii Kolesnykov <root@yurikoles.com>

--- Casks/kdeconnect.rb
+++ Casks/kdeconnect.rb
@@ -4,9 +4,9 @@ cask "kdeconnect" do
   # TODO: eventually also handle release versions (e.g. release-24.08 etc.)?
   base_url = "https://cdn.kde.org/ci-builds/network/kdeconnect-kde/master/macos-#{arch}"
 
-  version "5218"
-  sha256 arm:   "35eef0facd814f3503de43e935ae650f1616a0c0d7b39b7821ad1f1c83373063",
-         intel: "2708e6d6d13dca7104f5d5f4ddd6ce8736d714ad00e03253b55b9c56e1a265a4"
+  version "5234"
+  sha256 arm:   "15df86bbaa22c3e9a890c983d1035b5875371a7ddef9ce3f2d4176a9917e185c",
+         intel: "797f89ec5b945a34c74f0a822ff961ee83df29893afe38af82fea17273cbe757"
 
   url "#{base_url}/kdeconnect-kde-master-#{version}-macos-clang-#{arch}.dmg"
   name "KDE Connect"
