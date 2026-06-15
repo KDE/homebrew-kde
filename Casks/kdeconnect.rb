0cdf9eb05e29caeb92952afe6e8a0b39ede9d526
Yurii Kolesnykov <root@yurikoles.com>
2026-06-15 20:04:02 +0100

    kdeconnect 6292

--- Casks/kdeconnect.rb
+++ Casks/kdeconnect.rb
@@ -4,9 +4,9 @@ cask "kdeconnect" do
   # TODO: eventually also handle release versions (e.g. release-24.08 etc.)?
   base_url = "https://cdn.kde.org/ci-builds/network/kdeconnect-kde/master/macos-#{arch}"
 
-  version "6265"
-  sha256 arm:   "92f9dba399c8edabda74e0ec987b12f851bbca2cde3647d876255d6fb7d15b4d",
-         intel: "66b08ab269cda6928d9fb7305d3b61dd669bceeffffa22656ad1f438f40ede58"
+  version "6292"
+  sha256 arm:   "91115ec4df2261fe24b793772ebafd58f2e8759747ed3108f9e2f0d50470cac2",
+         intel: "7470c3c1d3df3d6cbc1023dffbcb4b1bac563d961885e46922ef508ae6c3964d"
 
   url "#{base_url}/kdeconnect-kde-master-#{version}-macos-clang-#{arch}.dmg"
   name "KDE Connect"
