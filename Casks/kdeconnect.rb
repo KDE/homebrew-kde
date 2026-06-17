4ed986ca5232e9c534f8d88480a295f2ab0a7dad
Yurii Kolesnykov <root@yurikoles.com>
2026-06-17 17:39:31 +0100

    kdeconnect migrated to homebrew/cask/kde-connect
    Signed-off-by: Yurii Kolesnykov <root@yurikoles.com>

--- Casks/kdeconnect.rb
+++ Casks/kdeconnect.rb
@@ -1,38 +1,17 @@
 cask "kdeconnect" do
-  arch arm: "arm64", intel: "x86_64"
+  version "26.04.2"
+  sha256 :no_check
 
-  # TODO: eventually also handle release versions (e.g. release-24.08 etc.)?
-  base_url = "https://cdn.kde.org/ci-builds/network/kdeconnect-kde/master/macos-#{arch}"
-
-  version "6292"
-  sha256 arm:   "91115ec4df2261fe24b793772ebafd58f2e8759747ed3108f9e2f0d50470cac2",
-         intel: "7470c3c1d3df3d6cbc1023dffbcb4b1bac563d961885e46922ef508ae6c3964d"
-
-  url "#{base_url}/kdeconnect-kde-master-#{version}-macos-clang-#{arch}.dmg"
-  name "KDE Connect"
-  desc "Enabling communication between all your devices"
+  url "https://invent.kde.org/packaging/homebrew-kde/-/raw/master/README.md"
+  name "KDE Connect Moved"
+  desc "Dummy transient cask, feel free to remove"
   homepage "https://kdeconnect.kde.org/"
 
   livecheck do
-    url base_url
-    regex(/href=.*?kdeconnect-kde-master-(\d+(?:)+)-macos-clang-#{arch}\.dmg/i)
+    skip "Meta package"
   end
 
-  depends_on macos: :ventura
+  depends_on cask: "homebrew/cask/kde-connect"
 
-  app "KDE Connect.app"
-  binary "#{appdir}/KDE Connect.app/Contents/MacOS/kdeconnect-cli",
-         target: "kdeconnect"
-
-  uninstall quit: "org.kde.kdeconnect"
-
-  zap trash: [
-    "~/Library/Application Support/kpeoplevcard",
-    "~/Library/Caches/kdeconnect.sms",
-    "~/Library/Preferences/kdeconnect",
-    "~/Library/Preferences/kdeconnect_runcommand",
-    "~/Library/Preferences/kdeconnect_sendnotifications",
-    "~/Library/Preferences/kdeconnect_share",
-    "~/Library/Preferences/org.kde.kdeconnect.plist",
-  ]
+  stage_only true
 end
